6433b9ae54affda4a291825b08bf7438e52b08fa
Kevin Kaldvansvik <kevin.k.kaldvansvik@gmail.com>
2020-03-31 13:12:24 +0200

    Revert "FSM will only transition to STOPPING if GOAL_STOP event is triggered"
    This reverts commit 89c139673e858dc5d70b7d97de52151951315513.

--- gnc/ctl/src/ctl.cc
+++ gnc/ctl/src/ctl.cc
@@ -69,7 +69,7 @@ Ctl::Ctl(ros::NodeHandle* nh, std::string const& name) :
     });
   // [1]
   fsm_.Add(NOMINAL,
-    GOAL_STOP,
+    GOAL_COMPLETE,
     [this](FSM::Event const& event) -> FSM::State {
       if (!Control(ff_msgs::ControlCommand::MODE_STOP))
         return Result(RESPONSE::CONTROL_FAILED);
@@ -355,7 +355,7 @@ void Ctl::TimerCallback(const ros::TimerEvent& event) {
     if (setpoint_ == segment_.end()) {
       mutex_segment_.unlock();
       NODELET_DEBUG_STREAM("Final setpoint " << idx);
-      return fsm_.Update(GOAL_STOP);
+      return fsm_.Update(GOAL_COMPLETE);
     }
     // Otherwise, we have at least one more setpoint
     msg.current = *setpoint_;
