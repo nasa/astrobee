5e5b854e5cadbf5a82a2cbcd40d5e646eac22061
Ryan Soussan <ryan.soussan@gmail.com>
2020-08-31 22:13:05 -0700

    updated other files with gtsam deb

--- scripts/setup/debians/build_install_debians_18_04.sh
+++ scripts/setup/debians/build_install_debians_18_04.sh
@@ -51,6 +51,14 @@ cd ${DEBIAN_LOC}
 ./build_dbow2.sh || exit 1
 sudo dpkg -i libdbow*_amd64.deb || exit 1
 
+cp ${DEBIAN_LOC}/files_18_04/gtsam_rules ${DEBIAN_LOC}/gtsam/rules
+cp ${DEBIAN_LOC}/files_18_04/gtsam_control ${DEBIAN_LOC}/gtsam/control
+cd ${DEBIAN_LOC}/gtsam
+sudo mk-build-deps -i -r -t "apt-get --no-install-recommends -y" control
+cd ${DEBIAN_LOC}
+./build_gtsam.sh || exit 1
+sudo dpkg -i libgtsam*_amd64.deb || exit 1
+
 cd ${DEBIAN_LOC}/decomputil
 sudo mk-build-deps -i -r -t "apt-get --no-install-recommends -y" control
 cd ${DEBIAN_LOC}
