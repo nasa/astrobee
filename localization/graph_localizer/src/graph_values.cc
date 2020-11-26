63bbb27efc398c90c9a956254b5ec5258a18dde3
Ryan Soussan <ryan.soussan@gmail.com>
2020-11-26 10:58:47 -0800

    fixed num features bug

--- localization/graph_localizer/src/graph_values.cc
+++ localization/graph_localizer/src/graph_values.cc
@@ -225,7 +225,7 @@ boost::optional<int> GraphValues::LatestCombinedNavStateKeyIndex() const {
   return timestamp_key_index_map_.crbegin()->second;
 }
 
-int GraphValues::NumFeatures() const { return timestamp_key_index_map_.size(); }
+int GraphValues::NumFeatures() const { return feature_id_key_map_.size(); }
 
 boost::optional<int> GraphValues::OldestCombinedNavStateKeyIndex() const {
   if (Empty()) {
