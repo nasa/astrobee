af330349d6d939bc1efc538b8815fcbb23cb24ab
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-11-30 12:23:08 -0800

    Doc improvements and minor changes to gds_simulator.py (#84)
    The gds_simulator was changed to accept either the number of the command to run, or the full command string

--- tools/gds_helper/src/gds_simulator.py
+++ tools/gds_helper/src/gds_simulator.py
@@ -332,7 +332,9 @@ def execute_action(selection):
     arg = CommandArg()
     arg.data_type = 5
     arg.s = apps[selection].apk_name
-
+    command     = "" # for when an the user supplies the command index
+    command_str = "" # For when the user enters the actual command
+    
     if option == 'a':
         # List commands
         clear()
@@ -362,13 +364,16 @@ def execute_action(selection):
             num_cmds = len(apps[selection].commands)
             print_app_cmd(selection)
             print str(num_cmds + 1) + ')  Exit program'
-            try:
-                command = input('\nType the number of the command you wish to send: ')
-            except:
-                print ' > Invalid entry'
-                time.sleep(1)
-                continue
 
+            command_str = raw_input('\nType the number of the command you wish to send or the command string: ')
+            try:
+                # Convert to an integer, the number of the command
+                command = int(command_str)
+                command_str = ""
+            except:
+                # Not an integer, so pass the full command and exit the loop
+                break
+            
             if command == num_cmds + 1:
                 return ACTION_EXIT
 
@@ -381,7 +386,12 @@ def execute_action(selection):
 
         arg2 = CommandArg()
         arg2.data_type = 5
-        arg2.s = apps[selection].commands[command].command
+        if command_str == "":
+            # find the command based on its number
+            arg2.s = apps[selection].commands[command].command
+        else:
+            arg2.s = command_str
+            
         clear()
         send_command('customGuestScience', [arg, arg2])
         print_gs_feedback()
