460570dbd9e6022f4d4564a5a167ed3725664e38
Ryan Soussan <ryan.soussan@gmail.com>
2020-12-01 14:09:43 -0800

    updated rotation test

--- localization/graph_localizer/test/test_rotation_factor.cc
+++ localization/graph_localizer/test/test_rotation_factor.cc
@@ -21,6 +21,13 @@
 #include <localization_measurements/feature_point.h>
 
 #include <gtsam/geometry/PinholePose.h>
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/linear/NoiseModel.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/PriorFactor.h>
+#include <gtsam/nonlinear/Values.h>
+#include <gtsam/slam/PoseTranslationPrior.h>
 
 #include <boost/make_shared.hpp>
 
@@ -29,6 +36,7 @@
 
 namespace gl = graph_localizer;
 namespace lm = localization_measurements;
+namespace sym = gtsam::symbol_shorthand;
 class RotationFactorAdderTester : public ::testing::Test {
  protected:
   virtual void SetUp() {
@@ -88,7 +96,7 @@ class RotationFactorAdderTester : public ::testing::Test {
   std::unique_ptr<gl::RotationFactorAdder> rotation_factor_adder_;
 };
 
-TEST_F(RotationFactorAdderTester, A) {
+TEST_F(RotationFactorAdderTester, CorrectRotation) {
   const gtsam::Pose3 world_T_cam_1;
   const gtsam::Point3 cam_1_t_cam_2(0.1, 0.1, 0.1);
   const gtsam::Rot3 cam_1_R_cam_2(gtsam::Rot3::RzRyRx(3.0 * M_PI / 180.0, 2.0 * M_PI / 180.0, 1.0 * M_PI / 180.0));
@@ -104,4 +112,39 @@ TEST_F(RotationFactorAdderTester, A) {
   const auto world_T_body_2 = world_T_cam_2 * rotation_factor_adder_params_.body_T_nav_cam.inverse();
   const auto body_1_R_body_2 = (world_T_body_1.inverse() * world_T_body_2).rotation();
   ASSERT_TRUE(rotation.equals(body_1_R_body_2));
+}
+
+TEST_F(RotationFactorAdderTester, CorrectOptimization) {
+  const gtsam::Pose3 world_T_cam_1;
+  const gtsam::Point3 cam_1_t_cam_2(0.1, 0.1, 0.1);
+  const gtsam::Rot3 cam_1_R_cam_2(gtsam::Rot3::RzRyRx(3.0 * M_PI / 180.0, 2.0 * M_PI / 180.0, 1.0 * M_PI / 180.0));
+  const gtsam::Pose3 cam_1_T_cam_2(cam_1_R_cam_2, cam_1_t_cam_2);
+  const gtsam::Pose3 world_T_cam_2 = world_T_cam_1 * cam_1_T_cam_2;
+  AddMeasurementsForTwoFrames(world_T_cam_1, world_T_cam_2, 30);
+  const auto factors_to_add_vec = rotation_factor_adder_->AddFactors(lm::FeaturePointsMeasurement());
+  auto rotation_factor =
+    dynamic_cast<gtsam::PoseRotationFactor*>(factors_to_add_vec.front().Get().front().factor.get());
+  ASSERT_TRUE(rotation_factor);
+  const gtsam::Pose3 world_T_body_1 = world_T_cam_1 * rotation_factor_adder_params_.body_T_nav_cam.inverse();
+  const gtsam::Pose3 world_T_body_2 = world_T_cam_2 * rotation_factor_adder_params_.body_T_nav_cam.inverse();
+  gtsam::Values values_;
+  values_.insert(sym::P(1), world_T_body_1);
+  // Use cam_1_T_cam_2 as a pertubation and make sure the optimizer corrects this
+  const gtsam::Pose3 world_T_perturbed_body_2 = world_T_body_2 * cam_1_T_cam_2;
+  values_.insert(sym::P(2), world_T_perturbed_body_2);
+  gtsam::NonlinearFactorGraph graph_;
+  const auto noise_1 = gtsam::noiseModel::Unit::Create(6);
+  const gtsam::PriorFactor<gtsam::Pose3> world_T_body_1_prior(sym::P(1), world_T_body_1, noise_1);
+  graph_.push_back(world_T_body_1_prior);
+  const auto noise_2 = gtsam::noiseModel::Unit::Create(3);
+  gtsam::PoseTranslationPrior<gtsam::Pose3> pose_translation_prior(sym::P(2), world_T_body_2.translation(), noise_2);
+  graph_.push_back(pose_translation_prior);
+  rotation_factor->keys() = gtsam::KeyVector({sym::P(1), sym::P(2)});
+  graph_.push_back(*rotation_factor);
+  gtsam::LevenbergMarquardtOptimizer optimizer(graph_, values_);
+  const auto optimized_values = optimizer.optimize();
+  const auto optimized_world_T_body_1 = optimized_values.at<gtsam::Pose3>(sym::P(1));
+  const auto optimized_world_T_body_2 = optimized_values.at<gtsam::Pose3>(sym::P(2));
+  ASSERT_TRUE(optimized_world_T_body_1.equals(world_T_body_1, 1e-6));
+  ASSERT_TRUE(optimized_world_T_body_2.equals(world_T_body_2, 1e-6));
 }
