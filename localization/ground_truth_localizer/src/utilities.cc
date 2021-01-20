b7c1b4abd59e70998e9dc65b9b8ffe2bd86b599c
Ryan Soussan <ryan.soussan@gmail.com>
2021-01-19 23:47:33 -0800

    updated ground truth localizer with mc loading

--- localization/ground_truth_localizer/src/utilities.cc
+++ localization/ground_truth_localizer/src/utilities.cc
@@ -18,30 +18,33 @@
 
 #include <ground_truth_localizer/utilities.h>
 #include <localization_common/utilities.h>
+#include <msg_conversions/msg_conversions.h>
 
 namespace ground_truth_localizer {
 namespace lc = localization_common;
+namespace mc = msg_conversions;
+
 Eigen::Isometry3d PoseFromMsg(const geometry_msgs::PoseStamped& pose_msg) {
   Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
-  pose.translation() = lc::VectorFromMsg<Eigen::Vector3d, geometry_msgs::Point>(pose_msg.pose.position);
+  pose.translation() = mc::VectorFromMsg<Eigen::Vector3d, geometry_msgs::Point>(pose_msg.pose.position);
   pose.linear() =
-    lc::RotationFromMsg<Eigen::Quaterniond, geometry_msgs::Quaternion>(pose_msg.pose.orientation).toRotationMatrix();
+    mc::RotationFromMsg<Eigen::Quaterniond, geometry_msgs::Quaternion>(pose_msg.pose.orientation).toRotationMatrix();
   return pose;
 }
 
 Twist TwistFromMsg(const geometry_msgs::TwistStamped& twist_msg) {
   Twist twist;
-  twist.linear_velocity = lc::VectorFromMsg<Eigen::Vector3d, geometry_msgs::Vector3>(twist_msg.twist.linear);
-  twist.angular_velocity = lc::VectorFromMsg<Eigen::Vector3d, geometry_msgs::Vector3>(twist_msg.twist.angular);
+  twist.linear_velocity = mc::VectorFromMsg<Eigen::Vector3d, geometry_msgs::Vector3>(twist_msg.twist.linear);
+  twist.angular_velocity = mc::VectorFromMsg<Eigen::Vector3d, geometry_msgs::Vector3>(twist_msg.twist.angular);
   return twist;
 }
 
 ff_msgs::EkfState LocStateMsg(const Eigen::Isometry3d& pose, const Twist& twist,
                               const localization_common::Time& timestamp) {
   ff_msgs::EkfState loc_msg;
-  lc::EigenPoseToMsg(pose, loc_msg.pose);
-  lc::VectorToMsg(twist.linear_velocity, loc_msg.velocity);
-  lc::VectorToMsg(twist.angular_velocity, loc_msg.omega);
+  mc::EigenPoseToMsg(pose, loc_msg.pose);
+  mc::VectorToMsg(twist.linear_velocity, loc_msg.velocity);
+  mc::VectorToMsg(twist.angular_velocity, loc_msg.omega);
   lc::TimeToHeader(timestamp, loc_msg.header);
   loc_msg.confidence = lc::Confidence::kGood;
   return loc_msg;
