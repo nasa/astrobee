e8f04e19de4f6c98f1fb2cde0d305f80f3885607
Marina Moreira <marina.moreira@nasa.gov>
2021-01-20 18:24:11 -0800

    restoring this file

--- scripts/build/build_debian.sh
+++ scripts/build/build_debian.sh
@@ -2,11 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# if [ -n "$(git status --porcelain)" ]; then 
-#   echo "You should not build Debians for a dirty source tree!"
-#   echo "Make sure all your changes are committed AND pushed to the server..."
-#   exit -1
-# fi
+if [ -n "$(git status --porcelain)" ]; then 
+  echo "You should not build Debians for a dirty source tree!"
+  echo "Make sure all your changes are committed AND pushed to the server..."
+  exit -1
+fi
 
 EXTRA_FLAGS="-b -a armhf"
 if [[ $* == *--config* ]]; then
@@ -14,5 +14,5 @@ if [[ $* == *--config* ]]; then
 fi
 
 pushd $DIR/../..
-DEB_BUILD_OPTIONS="parallel=8" debuild -e ARMHF_CHROOT_DIR -e ARMHF_TOOLCHAIN -us -uc $EXTRA_FLAGS
+DEB_BUILD_OPTIONS="parallel=20" debuild -e ARMHF_CHROOT_DIR -e ARMHF_TOOLCHAIN -us -uc $EXTRA_FLAGS
 popd > /dev/null
