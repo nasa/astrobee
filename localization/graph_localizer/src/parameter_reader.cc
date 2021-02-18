9d6644b1fd894de3ca4f009263d0be193df8af70
Ryan Soussan <ryan.soussan@gmail.com>
2021-02-18 14:29:38 -0800

    added max num points per factor for smart factor, increased feature duration to 1

--- localization/graph_localizer/src/parameter_reader.cc
+++ localization/graph_localizer/src/parameter_reader.cc
@@ -132,6 +132,7 @@ void LoadSmartProjectionFactorAdderParams(config_reader::ConfigReader& config,
   params.robust = mc::LoadBool(config, "smart_projection_adder_robust");
   params.max_num_factors = mc::LoadInt(config, "smart_projection_adder_max_num_factors");
   params.min_num_points = mc::LoadInt(config, "smart_projection_adder_min_num_points");
+  params.max_num_points_per_factor = mc::LoadInt(config, "smart_projection_adder_max_num_points_per_factor");
   params.rotation_only_fallback = mc::LoadBool(config, "smart_projection_adder_rotation_only_fallback");
   params.splitting = mc::LoadBool(config, "smart_projection_adder_splitting");
   params.scale_noise_with_num_points = mc::LoadBool(config, "smart_projection_adder_scale_noise_with_num_points");
