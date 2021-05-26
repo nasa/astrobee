32db2b3621d7c375c684f1b91ae66d1b386e8b21
Ryan Soussan <ryan.soussan@gmail.com>
2021-05-26 15:18:32 -0700

    initial handrail point to line factor commit

---
+++ localization/graph_localizer/include/graph_localizer/point_to_line_factor.h
@@ -0,0 +1,108 @@
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef GRAPH_LOCALIZER_POINT_TO_LINE_FACTOR_H_
+#define GRAPH_LOCALIZER_POINT_TO_LINE_FACTOR_H_
+
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Point3.h>
+#include <gtsam/nonlinear/NonlinearFactor.h>
+
+#include <string>
+
+namespace gtsam {
+class PointToLineFactor : public NoiseModelFactor1<gtsam::Pose3> {
+  typedef NoiseModelFactor1<Pose3> Base;
+  typedef PointToLineFactor This;
+
+ public:
+  PointToLineFactor() {}
+
+  // The line in world_T_line is oriented such that the z axis is along the line and is initialized with an arbitrary
+  // orientation about the z axis.  body_T_sensor is the sensor extrinsics and assumed to be static.
+  PointToLineFactor(const Point3& sensor_t_point, const Pose3& world_T_line, const Pose3& body_T_sensor,
+                    const SharedNoiseModel& model, Key pose_key)
+      : Base(model, pose_key),
+        sensor_t_point_(sensor_t_point),
+        world_T_line_(world_T_line),
+        body_T_sensor_(body_T_sensor) {}
+
+  gtsam::NonlinearFactor::shared_ptr clone() const override {
+    return boost::static_pointer_cast<gtsam::NonlinearFactor>(gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+  }
+
+  void print(const std::string& s = "", const KeyFormatter& keyFormatter = DefaultKeyFormatter) const override {
+    std::cout << s << "PointToLineFactor, z = ";
+    traits<Point3>::Print(sensor_t_point_);
+    traits<Pose3>::Print(world_T_line_);
+    traits<Pose3>::Print(body_T_sensor_);
+    Base::print("", keyFormatter);
+  }
+
+  bool equals(const NonlinearFactor& p, double tol = 1e-9) const override {
+    const This* e = dynamic_cast<const This*>(&p);
+    return e && Base::equals(p, tol) && traits<Point3>::Equals(this->sensor_t_point(), e->sensor_t_point(), tol) &&
+           traits<Pose3>::Equals(this->world_T_line(), e->world_T_line(), tol) &&
+           traits<Pose3>::Equals(this->body_T_sensor(), e->body_T_sensor(), tol);
+  }
+
+  Vector evaluateError(const Pose3& world_T_body, boost::optional<Matrix&> H = boost::none) const override {
+    // TODO: store line_T_world instead of world_T_line?
+    const Point3 line_t_point = world_T_line_.inverse() * world_T_body * body_T_sensor_ * sensor_t_point_;
+    if (H) {
+      // TODO: store this as well
+      const Pose3 sensor_T_body = body_T_sensor_.inverse();
+      const Pose3 line_T_sensor = world_T_line_.inverse() * world_T_body * body_T_sensor_;
+      const Rot3& line_R_sensor = line_T_sensor.rotation();
+      const Rot3& sensor_R_body = sensor_T_body.rotation();
+      const Point3& body_t_sensor = body_T_sensor_.translation();
+      //const Matrix A = line_R_sensor * (-1.0 * skewSymmetric(sensor_t_point_)) * sensor_R_body;
+      const Matrix3 s = skewSymmetric(sensor_t_point_);
+      const Matrix3 test = s* sensor_R_body;
+      const Matrix A = line_R_sensor * s * sensor_R_body;
+      const Matrix B = line_R_sensor * (I_3x3 + -1.0*skewSymmetric(sensor_R_body * body_t_sensor) * sensor_R_body);
+      // Remove last row as error does not account for z value in line_t_point
+      H << A.sub(0, 2, 0, 6), B.sub(0, 2, 0, 6);
+    }
+    return line_t_point.head<2>();
+  }
+ 
+  const Point3& sensor_t_point() const { return sensor_t_point_; }
+  const Pose3& world_T_line() const {return world_T_line_; }
+  const Pose3& body_T_sensor() const { return body_T_sensor_;}
+
+ private:
+  friend class boost::serialization::access;
+  template <class ARCHIVE>
+  void serialize(ARCHIVE& ar, const unsigned int /*version*/) {
+    ar& BOOST_SERIALIZATION_BASE_OBJECT_NVP(Base);
+    ar& BOOST_SERIALIZATION_NVP(sensor_t_point_);
+    ar& BOOST_SERIALIZATION_NVP(world_T_line_);
+    ar& BOOST_SERIALIZATION_NVP(body_T_sensor_);
+  }
+
+  Point3 sensor_t_point_;
+  Pose3 world_T_line_;
+  Pose3 body_T_sensor_;
+
+ public:
+  GTSAM_MAKE_ALIGNED_OPERATOR_NEW
+};
+}  // namespace gtsam
+
+#endif  // GRAPH_LOCALIZER_POINT_TO_LINE_FACTOR_H_
---
+++ localization/graph_localizer/test/test_point_to_line_factor.cc
@@ -0,0 +1,55 @@
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <graph_localizer/point_to_line_factor.h>
+#include <localization_common/logger.h>
+
+#include <gtsam/base/numericalDerivative.h>
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/linear/NoiseModel.h>
+
+#include <gtest/gtest.h>
+
+namespace {
+gtsam::Pose3 RandomPose() {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  gtsam::Rot3 rot = gtsam::Rot3::Random(rng);
+  gtsam::Point3 trans = Eigen::Vector3d::Random();
+  return gtsam::Pose3(rot, trans);
+}
+}  // namespace
+
+namespace sym = gtsam::symbol_shorthand;
+TEST(RotationFactorTester, Jacobian) {
+  for (int i = 0; i < 500; ++i) {
+    const gtsam::Point3 sensor_t_point = Eigen::Vector3d::Random();
+    const gtsam::Pose3 world_T_line = RandomPose();
+    const gtsam::Pose3 body_T_sensor = RandomPose();
+    const gtsam::Pose3 world_T_body = RandomPose();
+    const auto noise = gtsam::noiseModel::Unit::Create(2);
+    const gtsam::PointToLineFactor factor(sensor_t_point, world_T_line, body_T_sensor, noise, sym::P(0));
+    gtsam::Matrix H;
+    const auto factor_error = factor.evaluateError(world_T_body, H);
+    const auto numerical_H = gtsam::numericalDerivative11<gtsam::Vector3, gtsam::Pose3>(
+      boost::function<gtsam::Vector(const gtsam::Pose3&)>(
+        boost::bind(&gtsam::PointToLineFactor::evaluateError, factor, _1, boost::none)),
+      world_T_body, 1e-5);
+    ASSERT_TRUE(numerical_H.isApprox(H.matrix(), 1e-6));
+  }
+}
