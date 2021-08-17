a726c2ffae7103149b5684c8aa9adc6affc1b633
Ryan Soussan <ryan.soussan@gmail.com>
2021-08-17 13:48:59 -0700

    using makeimagemeasurement in depth odom display plugin

--- tools/localization_rviz_plugins/src/depth_odometry_image_display.cc
+++ tools/localization_rviz_plugins/src/depth_odometry_image_display.cc
@@ -89,24 +89,11 @@ void DepthOdometryImageDisplay::createCorrespondencesImage() {
   if (!source_image_msg || !target_image_msg) return;
   clearImageBuffer(source_time);
 
-  // TODO(rsoussan): make function for this, unify with loc graph display
-  cv_bridge::CvImagePtr source_cv_image;
-  try {
-    source_cv_image = cv_bridge::toCvCopy(source_image_msg, sensor_msgs::image_encodings::RGB8);
-  } catch (cv_bridge::Exception& e) {
-    LogError("cv_bridge exception: " << e.what());
-    return;
-  }
-  auto& source_image = source_cv_image->image;
-
-  cv_bridge::CvImagePtr target_cv_image;
-  try {
-    target_cv_image = cv_bridge::toCvCopy(target_image_msg, sensor_msgs::image_encodings::RGB8);
-  } catch (cv_bridge::Exception& e) {
-    LogError("cv_bridge exception: " << e.what());
-    return;
-  }
-  auto& target_image = target_cv_image->image;
+  auto source_image_measurement = lm::MakeImageMeasurement(source_image_msg, sensor_msgs::image_encodings::MONO16);
+  auto target_image_measurement = lm::MakeImageMeasurement(target_image_msg, sensor_msgs::image_encodings::MONO16);
+  if (!source_image_measurement || !target_image_measurement) return;
+  auto& source_image = source_image_measurement->image;
+  auto& target_image = target_image_measurement->image;
 
   // Create correspondence image
   // Draw source image above target image, add correspondences as points outlined
