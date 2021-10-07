5f7dcd116b440a6fba422867e9c5b11a55c61497
Ryan Soussan <ryan.soussan@gmail.com>
2021-10-07 13:53:17 -0700

    added point cloud with known correspondences aligner

---
+++ localization/depth_odometry/include/depth_odometry/point_cloud_with_known_correspondences_aligner.h
@@ -0,0 +1,121 @@
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+#ifndef DEPTH_ODOMETRY_POINT_CLOUD_WITH_KNOWN_CORRESPONDENCES_ALIGNER_H_
+#define DEPTH_ODOMETRY_POINT_CLOUD_WITH_KNOWN_CORRESPONDENCES_ALIGNER_H_
+
+#include <localization_common/pose_with_covariance.h>
+
+#include <Eigen/Core>
+
+#include <ceres/problem.h>
+#include <ceres/jet.h>
+#include <ceres/ceres.h>
+#include <ceres/solver.h>
+#include <ceres/cost_function.h>
+#include <ceres/loss_function.h>
+#include <ceres/autodiff_cost_function.h>
+
+
+namespace depth_odometry {
+ // Assumes compact quaternion parameterization for rotations
+  // TODO(rsoussan): Use exponential map with local parameterization and compact axis angle parameterization
+  template <typename T>
+  Eigen::Transform<T, 3, Eigen::Isometry> Isometry3(const T* isometry_data) {
+    Eigen::Map<const Eigen::Matrix<T, 3, 1>> compact_quaternion(isometry_data);
+    Eigen::Matrix<T, 3, 3> rotation;
+    // For a quaternion, sqrt(x^2+y^2+z^2+w^2) = 1
+    // Since a compact quaternion provides the x,y,z compenents, to recover w use:
+    // w^2 = 1 - (x^2 + y^2 + z^2)
+    const T w_squared = 1.0 - compact_quaternion.squaredNorm();
+    // Catch invalid quaternion
+    if (w_squared <= 0.0) {
+      rotation = Eigen::Matrix<T, 3, 3>::Identity();
+    } else {
+      const Eigen::Quaternion<T> quaternion(ceres::sqrt(w_squared), compact_quaternion[0], compact_quaternion[1],
+                                            compact_quaternion[2]);
+      rotation = Eigen::Matrix<T, 3, 3>(quaternion);
+    }
+    Eigen::Map<const Eigen::Matrix<T, 3, 1>> translation(&isometry_data[3]);
+    Eigen::Transform<T, 3, Eigen::Isometry> isometry_3;
+    isometry_3.linear() = rotation;
+    isometry_3.translation() = translation;
+    return isometry_3;
+  }
+
+class PointToPointError {
+ public:
+  PointToPointError(const Eigen::Vector3d& source_point, const Eigen::Vector3d& target_point)
+      : source_point_(source_point), target_point_(target_point) {}
+
+  template <typename T>
+  bool operator()(const T* relative_transform_data, T* point_to_point_error) const {
+    const auto relative_transform = Isometry3<T>(relative_transform_data);
+    // Compute error
+    const Eigen::Matrix<T, 3, 1> transformed_source_point = relative_transform * source_point_.cast<T>();
+    point_to_point_error[0] = transformed_source_point[0] - target_point_[0];
+    point_to_point_error[1] = transformed_source_point[1] - target_point_[1];
+    point_to_point_error[2] = transformed_source_point[2] - target_point_[2];
+    return true;
+  }
+
+ private:
+  Eigen::Vector3d source_point_;
+  Eigen::Vector3d target_point_;
+};
+
+
+class PointCloudWithKnownCorrespondencesAligner {
+public:
+PointCloudWithKnownCorrespondencesAligner();
+
+Eigen::Isometry3d Align(const std::vector<Eigen::Vector3d>& source_points, const std::vector<Eigen::Vector3d>& target_points, const Eigen::Isometry3d& initial_guess) const;
+
+localization_common::PoseWithCovariance ComputeRelativeTransform(const std::vector<Eigen::Vector3d>& source_points, const std::vector<Eigen::Vector3d>& target_points) const;
+
+Eigen::Isometry3d ComputeRelativeTransformUmeyama(const std::vector<Eigen::Vector3d>& source_points, const std::vector<Eigen::Vector3d>& target_points) const; 
+private:
+  // TODO(rsoussan): Add these functions to common place to share with intrinsics calibrator
+
+void AddCostFunction(const Eigen::Vector3d& source_point, const Eigen::Vector3d& target_point,
+                                           Eigen::Matrix<double, 6, 1>& relative_transform,
+                                           ceres::Problem& problem) const {
+  // TODO: pass this? delete at end?
+  ceres::LossFunction* huber_loss = new ceres::HuberLoss(1.345);
+  ceres::CostFunction* point_to_point_cost_function =
+    new ceres::AutoDiffCostFunction<PointToPointError, 3, 6>(new PointToPointError(source_point, target_point));
+  problem.AddResidualBlock(point_to_point_cost_function, huber_loss, relative_transform.data());
+}
+
+ 
+
+  static Eigen::Matrix<double, 6, 1> VectorFromIsometry3d(const Eigen::Isometry3d& isometry_3d){
+  Eigen::Quaterniond quaternion(isometry_3d.linear());
+  // Use normalized x,y,z components for compact quaternion
+  // TODO(rsoussan): Is this normalize call necessary?
+  quaternion.normalize();
+  Eigen::Vector3d compact_quaternion = quaternion.vec();
+  Eigen::Matrix<double, 6, 1> isometry_3d_vector;
+  isometry_3d_vector.head<3>() = compact_quaternion;
+  isometry_3d_vector.block<3, 1>(3, 0) = isometry_3d.translation();
+  return isometry_3d_vector;
+  }
+};
+
+}  // namespace depth_odometry
+
+#endif  // DEPTH_ODOMETRY_POINT_CLOUD_WITH_KNOWN_CORRESPONDENCES_ALIGNER_H_
---
+++ localization/depth_odometry/src/point_cloud_with_known_correspondences_aligner.cc
@@ -0,0 +1,75 @@
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <depth_odometry/point_cloud_with_known_correspondences_aligner.h>
+#include <localization_common/logger.h>
+#include <localization_common/timer.h>
+
+
+namespace depth_odometry {
+namespace lc = localization_common;
+
+PointCloudWithKnownCorrespondencesAligner::PointCloudWithKnownCorrespondencesAligner()/*const PointCloudWithKnownCorrespondencesAlignerParams& params)
+    : params_(params)*/ {}
+
+Eigen::Isometry3d PointCloudWithKnownCorrespondencesAligner::Align(const std::vector<Eigen::Vector3d>& source_points, const std::vector<Eigen::Vector3d>& target_points, const Eigen::Isometry3d& initial_guess) const {
+  Eigen::Matrix<double, 6, 1> relative_transform = VectorFromIsometry3d(initial_guess);
+  ceres::Problem problem;
+  problem.AddParameterBlock(relative_transform.data(), 6);
+  for (int i = 0; i < static_cast<int>(source_points.size()) && i < params_.max_num_matches; ++i) {
+      AddCostFunction(source_points[i], target_points[i], relative_transform, problem);
+  }
+
+  ceres::Solver::Options options;
+  options.linear_solver_type = ceres::DENSE_QR;
+  options.max_num_iterations = params_.max_num_iterations;
+  options.function_tolerance = params_.function_tolerance;
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+  std::cout << summary.FullReport() << "\n";
+  return Isometry3(relative_transform.data());
+}
+
+lc::PoseWithCovariance PointCloudWithKnownCorrespondencesAligner::ComputeRelativeTransform(const std::vector<Eigen::Vector3d>& source_points, const std::vector<Eigen::Vector3d>& target_points) const {
+  Eigen::Isometry3d initial_guess = Eigen::Isometry3d::Identity();
+  // TODO(rsoussan): add option to use umeyama as initial guess
+  const Eigen::Isometry3d relative_transform = Align(source_points, target_points, initial_guess);
+  return lc::PoseWithCovariance(relative_transform, Eigen::Matrix<double, 6, 6>::Zero());
+}
+
+Eigen::Isometry3d PointCloudWithKnownCorrespondencesAligner::ComputeRelativeTransformUmeyama(const std::vector<Eigen::Vector3d>& source_points, const std::vector<Eigen::Vector3d>& target_points) const {
+  // TODO(rsoussan): clean up naming
+  const int npts = static_cast <int> (source_points.size ());
+    Eigen::Matrix<double, 3, Eigen::Dynamic> cloud_src (3, npts);
+    Eigen::Matrix<double, 3, Eigen::Dynamic> cloud_tgt (3, npts);
+    for (int i = 0; i < npts; ++i) {
+      const auto& source_point = source_points[i];
+      cloud_src (0, i) = source_point.x();
+      cloud_src (1, i) = source_point.y();
+      cloud_src (2, i) = source_point.z();
+
+      const auto& target_point = target_points[i];
+      cloud_tgt (0, i) = target_point.x();
+      cloud_tgt (1, i) = target_point.y();
+      cloud_tgt (2, i) = target_point.z();
+    }
+
+    const Eigen::Matrix<double, 4, 4> relative_transform = Eigen::umeyama(cloud_src, cloud_tgt, false);
+    return Eigen::Isometry3d(relative_transform.matrix());
+}
+}  // namespace depth_odometry
