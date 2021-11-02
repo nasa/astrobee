1711d324a408f13c0f5cfe18998cfd3cd7df23b1
Ryan Soussan <ryan.soussan@gmail.com>
2021-11-01 20:11:23 -0700

    update make gt script

--- tools/graph_bag/scripts/make_groundtruth.py
+++ tools/graph_bag/scripts/make_groundtruth.py
@@ -26,67 +26,17 @@ import sys
 import make_groundtruth_map
 import utilities
 
-def create_groundtruth(bagfile, base_surf_map, maps_directory, world, robot_name):
-  os.mkdir("gt_images")
-  gt_images = os.path.abspath('gt_images')
-  extract_images_command = "rosrun localization_node extract_image_bag " + bagfile + " -use_timestamp_as_image_name -image_topic /mgt/img_sampler/nav_cam/image_record -output_directory gt_images"
-  utilities.run_command_and_save_output(extract_images_command, 'extract_images.txt')
-
-  select_images_command = "rosrun sparse_mapping select_images -density_factor 1.4 gt_images/*.jpg"
-  utilities.run_command_and_save_output(select_images_command, 'select_images.txt')
-
-  # Set environment variables
-  home = os.path.expanduser('~')
-  robot_config_file = os.path.join('config/robots', robot_name + '.config')
-  astrobee_path = os.path.join(home, 'astrobee/astrobee')
-  os.environ['ASTROBEE_RESOURCE_DIR'] = os.path.join(astrobee_path, 'resources')
-  os.environ['ASTROBEE_CONFIG_DIR'] = os.path.join(astrobee_path, 'config')
-  os.environ['ASTROBEE_ROBOT'] = os.path.join(astrobee_path, 'config/robots/bumble.config')
-  os.environ['ASTROBEE_WORLD'] = world
-
-  # Build groundtruth
-  build_map_command = 'rosrun sparse_mapping build_map gt_images/*jpg -output_map groundtruth.map -feature_detection -feature_matching -track_building -incremental_ba -bundle_adjustment -histogram_equalization -num_subsequent_images 100'
-  utilities.run_command_and_save_output(build_map_command, 'build_map.txt')
-
-  # Merge with base map
-  merge_map_command = 'rosrun sparse_mapping merge_maps ' + base_surf_map + ' groundtruth.map -output_map groundtruth.surf.map -num_image_overlaps_at_endpoints 100000000 -skip_bundle_adjustment'
-  utilities.run_command_and_save_output(merge_map_command, 'merge_map.txt')
-
-  # Link maps directory since conversion to BRISK map needs
-  # image files to appear to be in correct relative path
-  os.symlink(maps_directory, 'maps')
-  os.symlink(gt_images, 'maps/gt_images')
-
-  # Convert SURF to BRISK map
-  # Get full path to output file to avoid permission errors when running 
-  # command in maps directory
-  rebuild_output_file = os.path.join(os.getcwd(), 'rebuild_map_as_brisk_map.txt')
-  shutil.copyfile("groundtruth.surf.map", "groundtruth.brisk.map")
-  groundtruth_brisk_map = os.path.abspath('groundtruth.brisk.map')
-  gt_path = os.getcwd()
-  os.chdir('maps')
-  rebuild_map_command = 'rosrun sparse_mapping build_map -rebuild -histogram_equalization -output_map ' + groundtruth_brisk_map
-  utilities.run_command_and_save_output(rebuild_map_command, rebuild_output_file)
-  # Use gt_path since relative commands would now be wrt maps directory simlink
-  os.chdir(gt_path)
-
-  # Create vocabdb
-  shutil.copyfile("groundtruth.brisk.map", "groundtruth.brisk.vocabdb.map")
-  add_vocabdb_command = 'rosrun sparse_mapping build_map -vocab_db -output_map groundtruth.brisk.vocabdb.map'
-  utilities.run_command_and_save_output(add_vocabdb_command, 'build_vocabdb.txt')
-
-  # Remove simlinks
-  os.unlink('maps/gt_images')
-  os.unlink('maps')
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('bagfile')
   parser.add_argument('base_surf_map')
   parser.add_argument('maps_directory')
+  parser.add_argument('loc_map')
   parser.add_argument("config_path", help="Full path to config path.")
   parser.add_argument('-o', '--output-directory', default='groundtruth_creation_output')
   parser.add_argument('-w', '--world', default='iss')
+  parser.add_argument("-i", "--image-topic", default='/mgt/img_sampler/nav_cam/image_record', help="Image topic.")
   parser.add_argument('-r', '--robot-name', default='bumble')
   parser.add_argument('-m', '--map-name', default=None)
   parser.add_argument(
@@ -103,6 +53,9 @@ if __name__ == '__main__':
   if not os.path.isfile(args.base_surf_map):
     print('Base surf map ' + args.base_surf_map + ' does not exist.')
     sys.exit()
+  if not os.path.isfile(args.loc_map):
+    print('Loc map ' + args.loc_map + ' does not exist.')
+    sys.exit()
   if not os.path.isdir(args.maps_directory):
     print('Maps directory ' + args.maps_directory + ' does not exist.')
     sys.exit()
@@ -115,17 +68,28 @@ if __name__ == '__main__':
   maps_directory = os.path.abspath(args.maps_directory)
 
   map_name = args.map_name
+  bag_prefix = os.path.splitext(os.path.basename(bagfile))[0] 
   if not args.map_name:
-    map_name = os.path.splitext(os.path.basename(bagfile))[0] + '_groundtruth'
+    map_name = bag_prefix + '_groundtruth'
 
-  make_groundtruth_map.create_groundtruth(bagfile, base_surf_map, maps_directory, map_name, args.world, args.robot_name)
+  #make_groundtruth_map.create_groundtruth(bagfile, base_surf_map, maps_directory, map_name, args.world, args.robot_name)
 
-  # TODO: add output file and output csv file names!
+  robot_config = 'config/robots/' + args.robot_name + '.config'
+  groundtruth_bag = map_name + '_groundtruth.bag'
   groundtruth_map_file = map_name + '.brisk.vocabdb.map'
-  make_groudtruth_command = 'rosrun graph_bag run_graph_and_plot_results.py ' + bagfile + ' ' + groundtruth_map_file + ' ' + args.image_topic + ' ' + args.config_path + ' ' + args.robot_config + ' ' + args.world + ' ' + args.output_file + ' ' + args.output_csv_file
+  groundtruth_pdf = 'groundtruth.pdf'
+  groundtruth_csv = 'groundtruth.csv'
+  make_groundtruth_command = 'rosrun graph_bag run_graph_bag_and_plot_results.py ' + bagfile + ' ' + groundtruth_map_file + ' ' + args.config_path + ' -i ' + args.image_topic + ' -r ' + robot_config + ' -w ' + args.world + ' -o ' + groundtruth_bag + ' --output-file ' + groundtruth_pdf + ' --output-csv-file ' + groundtruth_csv 
   if not args.use_image_features:
     make_groundtruth_command += ' --generate-image-features'
   utilities.run_command_and_save_output(make_groundtruth_command, 'make_groundtruth.txt')
 
-  # TODO run loc with other map! plot results!
-  # copy last command! change map and ouput names!
+  loc_results_bag = bag_prefix + '_results.bag'
+  loc_pdf = 'loc_results.pdf'
+  loc_csv = 'loc_results.csv'
+  get_loc_results_command = 'rosrun graph_bag run_graph_bag_and_plot_results.py ' + bagfile + ' ' + args.loc_map + ' ' + args.config_path + ' -i ' + args.image_topic + ' -r ' + robot_config + ' -w ' + args.world + ' -o ' + loc_results_bag + ' --output-file ' + loc_pdf + ' --output-csv-file ' + loc_csv
+  if not args.use_image_features:
+    make_groundtruth_command += ' --generate-image-features'
+  utilities.run_command_and_save_output(get_loc_results_command, 'get_loc_results.txt')
+
+
