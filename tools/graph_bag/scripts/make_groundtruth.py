1d0e10da4b903cff4d6440c3138c986537f0c06f
Ryan Soussan <ryan.soussan@gmail.com>
2021-11-02 10:48:54 -0700

    formatted scripts

--- tools/graph_bag/scripts/make_groundtruth.py
+++ tools/graph_bag/scripts/make_groundtruth.py
@@ -27,64 +27,65 @@ import make_groundtruth_map
 import utilities
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('bagfile')
-  parser.add_argument('base_surf_map')
-  parser.add_argument('maps_directory')
-  parser.add_argument('loc_map')
-  parser.add_argument("config_path", help="Full path to config path.")
-  parser.add_argument('-o', '--output-directory', default='groundtruth_creation_output')
-  parser.add_argument('-w', '--world', default='iss')
-  parser.add_argument("-i", "--image-topic", default='/mgt/img_sampler/nav_cam/image_record', help="Image topic.")
-  parser.add_argument('-r', '--robot-name', default='bumble')
-  parser.add_argument('-m', '--map-name', default=None)
-  parser.add_argument(
-    "--generate-image-features",
-    dest="use_image_features",
-    action="store_false",
-    help="Use image features msgs from bagfile or generate features from images.",
-  )
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bagfile')
+    parser.add_argument('base_surf_map')
+    parser.add_argument('maps_directory')
+    parser.add_argument('loc_map')
+    parser.add_argument("config_path", help="Full path to config path.")
+    parser.add_argument('-o', '--output-directory', default='groundtruth_creation_output')
+    parser.add_argument('-w', '--world', default='iss')
+    parser.add_argument("-i", "--image-topic", default='/mgt/img_sampler/nav_cam/image_record', help="Image topic.")
+    parser.add_argument('-r', '--robot-name', default='bumble')
+    parser.add_argument('-m', '--map-name', default=None)
+    parser.add_argument(
+        "--generate-image-features",
+        dest="use_image_features",
+        action="store_false",
+        help="Use image features msgs from bagfile or generate features from images.",
+    )
 
-  args = parser.parse_args()
-  if not os.path.isfile(args.bagfile):
-    print('Bag file ' + args.bagfile + ' does not exist.')
-    sys.exit()
-  if not os.path.isfile(args.base_surf_map):
-    print('Base surf map ' + args.base_surf_map + ' does not exist.')
-    sys.exit()
-  if not os.path.isfile(args.loc_map):
-    print('Loc map ' + args.loc_map + ' does not exist.')
-    sys.exit()
-  if not os.path.isdir(args.maps_directory):
-    print('Maps directory ' + args.maps_directory + ' does not exist.')
-    sys.exit()
-  if os.path.isdir(args.output_directory):
-    print('Output directory ' + args.output_directory + ' already exists.')
-    sys.exit()
+    args = parser.parse_args()
+    if not os.path.isfile(args.bagfile):
+        print('Bag file ' + args.bagfile + ' does not exist.')
+        sys.exit()
+    if not os.path.isfile(args.base_surf_map):
+        print('Base surf map ' + args.base_surf_map + ' does not exist.')
+        sys.exit()
+    if not os.path.isfile(args.loc_map):
+        print('Loc map ' + args.loc_map + ' does not exist.')
+        sys.exit()
+    if not os.path.isdir(args.maps_directory):
+        print('Maps directory ' + args.maps_directory + ' does not exist.')
+        sys.exit()
+    if os.path.isdir(args.output_directory):
+        print('Output directory ' + args.output_directory + ' already exists.')
+        sys.exit()
 
-  bagfile = os.path.abspath(args.bagfile)
-  base_surf_map = os.path.abspath(args.base_surf_map)
-  maps_directory = os.path.abspath(args.maps_directory)
+    bagfile = os.path.abspath(args.bagfile)
+    base_surf_map = os.path.abspath(args.base_surf_map)
+    maps_directory = os.path.abspath(args.maps_directory)
 
-  map_name = args.map_name
-  bag_prefix = utilities.basename(bagfile)
-  if not args.map_name:
-    map_name = bag_prefix + '_groundtruth'
+    map_name = args.map_name
+    bag_prefix = utilities.basename(bagfile)
+    if not args.map_name:
+        map_name = bag_prefix + '_groundtruth'
 
-  make_groundtruth_map.create_groundtruth(bagfile, base_surf_map, maps_directory, map_name, args.world, args.robot_name)
+    make_groundtruth_map.create_groundtruth(bagfile, base_surf_map, maps_directory, map_name, args.world,
+                                            args.robot_name)
 
-  robot_config = 'config/robots/' + args.robot_name + '.config'
-  groundtruth_bag = map_name + '.bag'
-  groundtruth_map_file = map_name + '.brisk.vocabdb.map'
-  groundtruth_pdf = 'groundtruth.pdf'
-  groundtruth_csv = 'groundtruth.csv'
-  make_groundtruth_command = 'rosrun graph_bag run_graph_bag_and_plot_results.py ' + bagfile + ' ' + groundtruth_map_file + ' ' + args.config_path + ' -i ' + args.image_topic + ' -r ' + robot_config + ' -w ' + args.world + ' -o ' + groundtruth_bag + ' --output-file ' + groundtruth_pdf + ' --output-csv-file ' + groundtruth_csv + ' --generate-image-features'
-  utilities.run_command_and_save_output(make_groundtruth_command, 'make_groundtruth.txt')
+    robot_config = 'config/robots/' + args.robot_name + '.config'
+    groundtruth_bag = map_name + '.bag'
+    groundtruth_map_file = map_name + '.brisk.vocabdb.map'
+    groundtruth_pdf = 'groundtruth.pdf'
+    groundtruth_csv = 'groundtruth.csv'
+    make_groundtruth_command = 'rosrun graph_bag run_graph_bag_and_plot_results.py ' + bagfile + ' ' + groundtruth_map_file + ' ' + args.config_path + ' -i ' + args.image_topic + ' -r ' + robot_config + ' -w ' + args.world + ' -o ' + groundtruth_bag + ' --output-file ' + groundtruth_pdf + ' --output-csv-file ' + groundtruth_csv + ' --generate-image-features'
+    utilities.run_command_and_save_output(make_groundtruth_command, 'make_groundtruth.txt')
 
-  loc_results_bag = bag_prefix + '_results.bag'
-  loc_pdf = 'loc_results.pdf'
-  loc_csv = 'loc_results.csv'
-  get_loc_results_command = 'rosrun graph_bag run_graph_bag_and_plot_results.py ' + bagfile + ' ' + args.loc_map + ' ' + args.config_path + ' -i ' + args.image_topic + ' -r ' + robot_config + ' -w ' + args.world + ' -o ' + loc_results_bag + ' --output-file ' + loc_pdf + ' --output-csv-file ' + loc_csv + ' -g ' + groundtruth_bag
-  if not args.use_image_features:
-    make_groundtruth_command += ' --generate-image-features'
-  utilities.run_command_and_save_output(get_loc_results_command, 'get_loc_results.txt')
+    loc_results_bag = bag_prefix + '_results.bag'
+    loc_pdf = 'loc_results.pdf'
+    loc_csv = 'loc_results.csv'
+    get_loc_results_command = 'rosrun graph_bag run_graph_bag_and_plot_results.py ' + bagfile + ' ' + args.loc_map + ' ' + args.config_path + ' -i ' + args.image_topic + ' -r ' + robot_config + ' -w ' + args.world + ' -o ' + loc_results_bag + ' --output-file ' + loc_pdf + ' --output-csv-file ' + loc_csv + ' -g ' + groundtruth_bag
+    if not args.use_image_features:
+        make_groundtruth_command += ' --generate-image-features'
+    utilities.run_command_and_save_output(get_loc_results_command, 'get_loc_results.txt')
--- tools/graph_bag/scripts/run_graph_bag_and_plot_results.py
+++ tools/graph_bag/scripts/run_graph_bag_and_plot_results.py
@@ -24,59 +24,59 @@ import sys
 import utilities
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("bagfile", help="Input bagfile.")
-  parser.add_argument("map_file", help="Map file.")
-  parser.add_argument("config_path", help="Full path to config path.")
-  parser.add_argument("-r",
-                      "--robot-config",
-                      default='config/robots/bumble.config',
-                      help="Relative path to robot config.")
-  parser.add_argument("-i", "--image-topic", default='/mgt/img_sampler/nav_cam/image_record', help="Image topic.")
-  parser.add_argument("-o", "--output-bagfile", default='results.bag', help="Output bagfile.")
-  parser.add_argument('-w', '--world', default='iss')
-  parser.add_argument(
-    "--generate-image-features",
-    dest="use_image_features",
-    action="store_false",
-    help="Use image features msgs from bagfile or generate features from images.",
-  )
-  parser.add_argument("--output-file", default="output.pdf")
-  parser.add_argument("--output-csv-file", default="results.csv")
-  parser.add_argument("-g", "--groundtruth-bagfile", default=None)
-  parser.add_argument(
-    "--directory",
-    default=None,
-    help=
-    "Full path to output directory where files will be saved. If not specified, timestamped directory will be created in current path.",
-  )
-  args = parser.parse_args()
-  if not os.path.isfile(args.bagfile):
-    print('Bag file ' + args.bagfile + ' does not exist.')
-    sys.exit()
-  if not os.path.isfile(args.map_file):
-    print('Map file ' + args.map_file + ' does not exist.')
-    sys.exit()
-  if os.path.isfile(args.output_file):
-    print('Output file ' + args.output_file + ' already exist.')
-    sys.exit()
-  if os.path.isfile(args.output_csv_file):
-    print('Output csv file ' + args.output_csv_file + ' already exist.')
-    sys.exit()
-  if args.groundtruth_bagfile and not os.path.isfile(args.groundtruth_bagfile):
-    print('Groundtruth bag ' + args.groundtruth_bagfile + ' does not exist.')
-    sys.exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bagfile", help="Input bagfile.")
+    parser.add_argument("map_file", help="Map file.")
+    parser.add_argument("config_path", help="Full path to config path.")
+    parser.add_argument("-r",
+                        "--robot-config",
+                        default='config/robots/bumble.config',
+                        help="Relative path to robot config.")
+    parser.add_argument("-i", "--image-topic", default='/mgt/img_sampler/nav_cam/image_record', help="Image topic.")
+    parser.add_argument("-o", "--output-bagfile", default='results.bag', help="Output bagfile.")
+    parser.add_argument('-w', '--world', default='iss')
+    parser.add_argument(
+        "--generate-image-features",
+        dest="use_image_features",
+        action="store_false",
+        help="Use image features msgs from bagfile or generate features from images.",
+    )
+    parser.add_argument("--output-file", default="output.pdf")
+    parser.add_argument("--output-csv-file", default="results.csv")
+    parser.add_argument("-g", "--groundtruth-bagfile", default=None)
+    parser.add_argument(
+        "--directory",
+        default=None,
+        help=
+        "Full path to output directory where files will be saved. If not specified, timestamped directory will be created in current path.",
+    )
+    args = parser.parse_args()
+    if not os.path.isfile(args.bagfile):
+        print('Bag file ' + args.bagfile + ' does not exist.')
+        sys.exit()
+    if not os.path.isfile(args.map_file):
+        print('Map file ' + args.map_file + ' does not exist.')
+        sys.exit()
+    if os.path.isfile(args.output_file):
+        print('Output file ' + args.output_file + ' already exist.')
+        sys.exit()
+    if os.path.isfile(args.output_csv_file):
+        print('Output csv file ' + args.output_csv_file + ' already exist.')
+        sys.exit()
+    if args.groundtruth_bagfile and not os.path.isfile(args.groundtruth_bagfile):
+        print('Groundtruth bag ' + args.groundtruth_bagfile + ' does not exist.')
+        sys.exit()
 
-  bagfile = os.path.abspath(args.bagfile)
-  map_file = os.path.abspath(args.map_file)
+    bagfile = os.path.abspath(args.bagfile)
+    map_file = os.path.abspath(args.map_file)
 
-  # Run localizer
-  run_graph_bag_command = 'rosrun graph_bag run_graph_bag ' + bagfile + ' ' + map_file + ' ' + args.config_path + ' -r ' + args.robot_config + ' -w ' + args.world + ' --use-image-features ' + str(
-    args.use_image_features) + ' -o ' + args.output_bagfile
-  utilities.run_command_and_save_output(run_graph_bag_command, 'run_graph_bag_command.txt')
+    # Run localizer
+    run_graph_bag_command = 'rosrun graph_bag run_graph_bag ' + bagfile + ' ' + map_file + ' ' + args.config_path + ' -r ' + args.robot_config + ' -w ' + args.world + ' --use-image-features ' + str(
+        args.use_image_features) + ' -o ' + args.output_bagfile
+    utilities.run_command_and_save_output(run_graph_bag_command, 'run_graph_bag_command.txt')
 
-  # Plot results
-  plot_results_command = 'rosrun graph_bag plot_results_main.py ' + bagfile + ' --output-file ' + args.output_file + ' --output-csv-file ' + args.output_csv_file
-  if args.groundtruth_bagfile:
-    plot_results_command += ' -g ' + args.groundtruth_bagfile
-  utilities.run_command_and_save_output(plot_results_command, 'plot_results_command.txt')
+    # Plot results
+    plot_results_command = 'rosrun graph_bag plot_results_main.py ' + bagfile + ' --output-file ' + args.output_file + ' --output-csv-file ' + args.output_csv_file
+    if args.groundtruth_bagfile:
+        plot_results_command += ' -g ' + args.groundtruth_bagfile
+    utilities.run_command_and_save_output(plot_results_command, 'plot_results_command.txt')
