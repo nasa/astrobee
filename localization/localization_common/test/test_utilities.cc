06a164dee27432d110f3f77ac5500c2a391914a8
Ryan Soussan <ryan.soussan@gmail.com>
2021-11-08 11:57:23 -0800

    added loc common tests

---
+++ localization/localization_common/test/test_utilities.cc
@@ -0,0 +1,33 @@
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <localization_common/logger.h>
+#include <localization_common/test_utilities.h>
+#include <localization_common/utilities.h>
+
+#include <gtest/gtest.h>
+
+namespace lc = localization_common;
+TEST(UtilitiesTester, Deg2Rad2Deg) {
+  for (int i = 0; i < 500; ++i) {
+    const double degrees = lc::RandomDouble(-360.0, 360.0);
+    const double radians = lc::Deg2Rad(degrees);
+    const double degrees_again = lc::Rad2Deg(radians);
+    ASSERT_NEAR(degrees, degrees_again, 1e-6);
+  }
+}
