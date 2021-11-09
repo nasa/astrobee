c656d6d7794b7d6de186558d45af0f6ad1da65cf
Ryan Soussan <ryan.soussan@gmail.com>
2021-11-09 14:04:12 -0800

    added rad and radtan distorters to cam reproj estimates

--- tools/calibration/test/test_reprojection_pose_estimate.cc
+++ tools/calibration/test/test_reprojection_pose_estimate.cc
@@ -76,6 +76,52 @@ TEST(ReprojectionPoseEstimateTester, RandomFrontFacingPosesRandomIntrinsicsRando
   }
 }
 
+TEST(ReprojectionPoseEstimateTester, RandomFrontFacingPosesRandomIntrinsicsRandomPointsRadDistortion) {
+  const auto params = ca::DefaultReprojectionPoseEstimateParams();
+  const double initial_estimate_translation_noise = 0.1;
+  const double initial_estimate_rotation_noise = 0.1;
+  const int num_points = 20;
+  std::vector<int> initial_inliers(num_points);
+  // Fill inliers with all indices
+  std::iota(initial_inliers.begin(), initial_inliers.end(), 0);
+  for (int i = 0; i < 500; ++i) {
+    const auto distortion = ca::RandomRadDistortion();
+    const auto correspondences = ca::RegistrationCorrespondences<oc::RadDistorter>(
+      ca::RandomFrontFacingPose(), lc::RandomIntrinsics(), ca::RandomFrontFacingPoints(num_points), distortion);
+    const Eigen::Isometry3d noisy_initial_estimate = lc::AddNoiseToIsometry3d(
+      correspondences.camera_T_target(), initial_estimate_translation_noise, initial_estimate_rotation_noise);
+    const auto pose_estimate = ca::ReprojectionPoseEstimateWithInitialEstimate<oc::RadDistorter>(
+      correspondences.correspondences().image_points, correspondences.correspondences().points_3d,
+      correspondences.intrinsics(), distortion, params, noisy_initial_estimate, initial_inliers);
+    ASSERT_TRUE(pose_estimate != boost::none);
+    ASSERT_TRUE(pose_estimate->first.matrix().isApprox(correspondences.camera_T_target().matrix(), 1e-6));
+    ASSERT_TRUE(pose_estimate->second.size() == num_points);
+  }
+}
+
+TEST(ReprojectionPoseEstimateTester, RandomFrontFacingPosesRandomIntrinsicsRandomPointsRadTanDistortion) {
+  const auto params = ca::DefaultReprojectionPoseEstimateParams();
+  const double initial_estimate_translation_noise = 0.1;
+  const double initial_estimate_rotation_noise = 0.1;
+  const int num_points = 20;
+  std::vector<int> initial_inliers(num_points);
+  // Fill inliers with all indices
+  std::iota(initial_inliers.begin(), initial_inliers.end(), 0);
+  for (int i = 0; i < 500; ++i) {
+    const auto distortion = ca::RandomRadTanDistortion();
+    const auto correspondences = ca::RegistrationCorrespondences<oc::RadTanDistorter>(
+      ca::RandomFrontFacingPose(), lc::RandomIntrinsics(), ca::RandomFrontFacingPoints(num_points), distortion);
+    const Eigen::Isometry3d noisy_initial_estimate = lc::AddNoiseToIsometry3d(
+      correspondences.camera_T_target(), initial_estimate_translation_noise, initial_estimate_rotation_noise);
+    const auto pose_estimate = ca::ReprojectionPoseEstimateWithInitialEstimate<oc::RadTanDistorter>(
+      correspondences.correspondences().image_points, correspondences.correspondences().points_3d,
+      correspondences.intrinsics(), distortion, params, noisy_initial_estimate, initial_inliers);
+    ASSERT_TRUE(pose_estimate != boost::none);
+    ASSERT_TRUE(pose_estimate->first.matrix().isApprox(correspondences.camera_T_target().matrix(), 1e-6));
+    ASSERT_TRUE(pose_estimate->second.size() == num_points);
+  }
+}
+
 TEST(ReprojectionPoseEstimateTester, RandomFrontFacingPosesRandomIntrinsicsTargetPoints) {
   const auto params = ca::DefaultReprojectionPoseEstimateParams();
   const double initial_estimate_translation_noise = 0.1;
