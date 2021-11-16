c9c4c7a1db1e9e4945a65993794948e748f6a4a1
Ryan Soussan <ryan.soussan@gmail.com>
2021-11-16 11:02:40 -0800

    updated imu aug test with const accel test class

--- localization/imu_augmentor/test/test_imu_augmentor.cc
+++ localization/imu_augmentor/test/test_imu_augmentor.cc
@@ -26,74 +26,96 @@
 namespace ia = imu_augmentor;
 namespace lc = localization_common;
 namespace lm = localization_measurements;
-TEST(IMUAugmentorTester, PimPredictConstantAcceleration) {
-  const ia::ImuAugmentorParams params = ia::DefaultImuAugmentorParams();
-  ia::ImuAugmentor imu_augmentor(params);
-  const double acceleration_i = 0.01;
-  const Eigen::Vector3d acceleration(acceleration_i, acceleration_i, acceleration_i);
-  constexpr double time_increment = 1 / 125.0;
+
+class ConstantAccelerationTest : public ::testing::Test {
+ public:
   // Start at time increment so first IMU measurement is after starting combined nav state time
-  const lc::Time start_time = time_increment;
-  constexpr int num_measurements = 20;
-  const std::vector<lm::ImuMeasurement> imu_measurements =
-    ia::ConstantAccelerationMeasurements(acceleration, num_measurements, start_time, time_increment);
-  for (const auto& imu_measurement : imu_measurements) {
-    imu_augmentor.BufferImuMeasurement(imu_measurement);
+  ConstantAccelerationTest()
+      : acceleration_i_(0.1),
+        acceleration_(acceleration_i_, acceleration_i_, acceleration_i_),
+        time_increment_(1.0 / 125.0),
+        start_time_(time_increment_),
+        num_measurements_(20) {
+    const ia::ImuAugmentorParams params = ia::DefaultImuAugmentorParams();
+    imu_augmentor_.reset(new ia::ImuAugmentor(params));
   }
 
-  // Augmenting with all IMU Measurements
-  {
-    const lc::CombinedNavState initial_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
-                                             gtsam::imuBias::ConstantBias(), 0);
-    lc::CombinedNavState imu_augmented_state = initial_state;
-
-    imu_augmentor.PimPredict(initial_state, imu_augmented_state);
-    EXPECT_NEAR(imu_augmented_state.timestamp(), num_measurements * time_increment, 1e-6);
-    const double expected_velocity_i = acceleration_i * num_measurements * time_increment;
-    const gtsam::Vector3 expected_velocity(expected_velocity_i, expected_velocity_i, expected_velocity_i);
-    // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
-    EXPECT_TRUE(imu_augmented_state.velocity().matrix().isApprox(expected_velocity.matrix(), 1e-6));
-    // x = 1/2*a*t^2
-    const double expected_position_i = acceleration_i * 0.5 * std::pow(num_measurements * time_increment, 2);
-    const gtsam::Vector3 expected_position(expected_position_i, expected_position_i, expected_position_i);
-    // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
-    EXPECT_TRUE(imu_augmented_state.pose().translation().matrix().isApprox(expected_position.matrix(), 1e-6));
+  void SetUp() final {
+    const std::vector<lm::ImuMeasurement> imu_measurements =
+      ia::ConstantAccelerationMeasurements(acceleration_, num_measurements_, start_time_, time_increment_);
+    for (const auto& imu_measurement : imu_measurements) {
+      imu_augmentor_->BufferImuMeasurement(imu_measurement);
+    }
   }
 
-  // Augmenting with half of IMU Measurements
-  {
-    const lc::CombinedNavState initial_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
-                                             gtsam::imuBias::ConstantBias(), 0);
-    const lc::Time imu_augmented_state_start_time = num_measurements / 2 * time_increment;
-    lc::CombinedNavState imu_augmented_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
-                                             gtsam::imuBias::ConstantBias(), imu_augmented_state_start_time);
+  ia::ImuAugmentor& imu_augmentor() { return *imu_augmentor_; }
 
-    imu_augmentor.PimPredict(initial_state, imu_augmented_state);
-    EXPECT_NEAR(imu_augmented_state.timestamp(), num_measurements * time_increment, 1e-6);
-    const double expected_velocity_i = acceleration_i * num_measurements / 2 * time_increment;
-    const gtsam::Vector3 expected_velocity(expected_velocity_i, expected_velocity_i, expected_velocity_i);
-    // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
-    EXPECT_TRUE(imu_augmented_state.velocity().matrix().isApprox(expected_velocity.matrix(), 1e-6));
-    // x = 1/2*a*t^2
-    const double expected_position_i = acceleration_i * 0.5 * std::pow(num_measurements / 2 * time_increment, 2);
-    const gtsam::Vector3 expected_position(expected_position_i, expected_position_i, expected_position_i);
-    // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
-    EXPECT_TRUE(imu_augmented_state.pose().translation().matrix().isApprox(expected_position.matrix(), 1e-6));
-  }
+  double acceleration_i() { return acceleration_i_; }
 
-  // Augmenting with all IMU Measurements, starting bias
-  {
-    const lc::CombinedNavState initial_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
-                                             gtsam::imuBias::ConstantBias(acceleration, gtsam::Vector3::Zero()), 0);
-    lc::CombinedNavState imu_augmented_state = initial_state;
+  const Eigen::Vector3d& acceleration() { return acceleration_; }
 
-    imu_augmentor.PimPredict(initial_state, imu_augmented_state);
-    EXPECT_NEAR(imu_augmented_state.timestamp(), num_measurements * time_increment, 1e-6);
-    // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
-    EXPECT_TRUE(imu_augmented_state.velocity().matrix().isApprox(gtsam::Vector3::Zero().matrix(), 1e-6));
-    // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
-    EXPECT_TRUE(imu_augmented_state.pose().translation().matrix().isApprox(gtsam::Vector3::Zero().matrix(), 1e-6));
-  }
+  double time_increment() { return time_increment_; }
+
+  int num_measurements() { return num_measurements_; }
+
+ private:
+  std::unique_ptr<ia::ImuAugmentor> imu_augmentor_;
+  const double acceleration_i_;
+  const Eigen::Vector3d acceleration_;
+  const double time_increment_;
+  const lc::Time start_time_;
+  const int num_measurements_;
+};
+
+TEST_F(ConstantAccelerationTest, AddAllMeasurements) {
+  const lc::CombinedNavState initial_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
+                                           gtsam::imuBias::ConstantBias(), 0);
+  lc::CombinedNavState imu_augmented_state = initial_state;
+  imu_augmentor().PimPredict(initial_state, imu_augmented_state);
+
+  EXPECT_NEAR(imu_augmented_state.timestamp(), num_measurements() * time_increment(), 1e-6);
+  const double expected_velocity_i = acceleration_i() * num_measurements() * time_increment();
+  const gtsam::Vector3 expected_velocity(expected_velocity_i, expected_velocity_i, expected_velocity_i);
+  // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
+  EXPECT_TRUE(imu_augmented_state.velocity().matrix().isApprox(expected_velocity.matrix(), 1e-6));
+  // x = 1/2*a*t^2
+  const double expected_position_i = acceleration_i() * 0.5 * std::pow(num_measurements() * time_increment(), 2);
+  const gtsam::Vector3 expected_position(expected_position_i, expected_position_i, expected_position_i);
+  // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
+  EXPECT_TRUE(imu_augmented_state.pose().translation().matrix().isApprox(expected_position.matrix(), 1e-6));
+}
+
+TEST_F(ConstantAccelerationTest, AddHalfOfMeasurements) {
+  const lc::CombinedNavState initial_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
+                                           gtsam::imuBias::ConstantBias(), 0);
+  const lc::Time imu_augmented_state_start_time = num_measurements() / 2 * time_increment();
+  lc::CombinedNavState imu_augmented_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
+                                           gtsam::imuBias::ConstantBias(), imu_augmented_state_start_time);
+  imu_augmentor().PimPredict(initial_state, imu_augmented_state);
+
+  EXPECT_NEAR(imu_augmented_state.timestamp(), num_measurements() * time_increment(), 1e-6);
+  const double expected_velocity_i = acceleration_i() * num_measurements() / 2 * time_increment();
+  const gtsam::Vector3 expected_velocity(expected_velocity_i, expected_velocity_i, expected_velocity_i);
+  // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
+  EXPECT_TRUE(imu_augmented_state.velocity().matrix().isApprox(expected_velocity.matrix(), 1e-6));
+  // x = 1/2*a*t^2
+  const double expected_position_i = acceleration_i() * 0.5 * std::pow(num_measurements() / 2 * time_increment(), 2);
+  const gtsam::Vector3 expected_position(expected_position_i, expected_position_i, expected_position_i);
+  // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
+  EXPECT_TRUE(imu_augmented_state.pose().translation().matrix().isApprox(expected_position.matrix(), 1e-6));
+}
+
+TEST_F(ConstantAccelerationTest, AddAllMeasurementsWithAccelBias) {
+  const lc::CombinedNavState initial_state(gtsam::Pose3::identity(), gtsam::Velocity3::Zero(),
+                                           gtsam::imuBias::ConstantBias(acceleration(), gtsam::Vector3::Zero()), 0);
+  lc::CombinedNavState imu_augmented_state = initial_state;
+  imu_augmentor().PimPredict(initial_state, imu_augmented_state);
+
+  EXPECT_NEAR(imu_augmented_state.timestamp(), num_measurements() * time_increment(), 1e-6);
+  // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
+  EXPECT_TRUE(imu_augmented_state.velocity().matrix().isApprox(gtsam::Vector3::Zero().matrix(), 1e-6));
+  // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
+  EXPECT_TRUE(imu_augmented_state.pose().translation().matrix().isApprox(gtsam::Vector3::Zero().matrix(), 1e-6));
 }
 
 /*TEST(IMUAugmentorTester, PimPredictConstantAngularVelocity) {
