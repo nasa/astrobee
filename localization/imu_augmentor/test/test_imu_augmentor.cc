e6baae9e65fdc297ba82b046d5045c4c1e610fb7
Ryan Soussan <ryan.soussan@gmail.com>
2021-11-16 08:42:15 -0800

    updated imu aug test

--- localization/imu_augmentor/test/test_imu_augmentor.cc
+++ localization/imu_augmentor/test/test_imu_augmentor.cc
@@ -29,8 +29,8 @@ namespace lm = localization_measurements;
 TEST(IMUAugmentorTester, PimPredictConstantAcceleration) {
   const ia::ImuAugmentorParams params = ia::DefaultImuAugmentorParams();
   ia::ImuAugmentor imu_augmentor(params);
-  // Pass Acceleration vector and time increment to const accel measurements!! (AAAAA)
-  const Eigen::Vector3d acceleration(0.01, 0.01, 0.01);
+  const double acceleration_i = 0.01;
+  const Eigen::Vector3d acceleration(acceleration_i, acceleration_i, acceleration_i);
   constexpr double time_increment = 1 / 125.0;
   // Start at time increment so first IMU measurement is after starting combined nav state time
   const lc::Time start_time = time_increment;
@@ -45,7 +45,11 @@ TEST(IMUAugmentorTester, PimPredictConstantAcceleration) {
   lc::CombinedNavState imu_augmented_state = initial_state;
 
   imu_augmentor.PimPredict(initial_state, imu_augmented_state);
-  ASSERT_NEAR(imu_augmented_state.timestamp(), num_measurements * time_increment, 1e-6);
+  EXPECT_NEAR(imu_augmented_state.timestamp(), num_measurements * time_increment, 1e-6);
+  const double expected_velocity_i = acceleration_i * num_measurements;
+  const gtsam::Vector3 expected_velocity(expected_velocity_i, expected_velocity_i, expected_velocity_i);
+  // TODO(rsoussan): Replace this with assert pred2 with eigen comparisson when other pr merged
+  EXPECT_TRUE(imu_augmented_state.velocity().matrix().isApprox(expected_velocity.matrix(), 1e-6));
   // make sure position and velocity are correct!! (AC)
 }
 
