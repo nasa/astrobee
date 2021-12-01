e7251f0efc9e2f086f5c32e6b4420a8be8f620b4
Ryan Soussan <ryan.soussan@gmail.com>
2021-12-01 12:11:45 -0800

    added symmetric cost test

--- localization/point_cloud_common/test/test_point_to_plane_icp.cc
+++ localization/point_cloud_common/test/test_point_to_plane_icp.cc
@@ -30,9 +30,31 @@
 namespace lc = localization_common;
 namespace pc = point_cloud_common;
 
-TEST(PointToPlaneICPTester, PerfectEstimate) {
+TEST(PointToPlaneICPTester, NoisyInitialEstimate) {
   const auto params = pc::DefaultPointToPlaneICPParams();
-  constexpr double search_radius = 0.03;
+  constexpr double translation_stddev = 0.01;
+  constexpr double rotation_stddev = 0.01;
+  pc::PointToPlaneICP<pcl::PointNormal> icp(params);
+  for (int i = 0; i < 50; ++i) {
+    const auto a_T_points_and_normals = pc::CubicPoints();
+    const auto b_T_a = lc::RandomIsometry3d();
+    const auto source_cloud_with_normals =
+      pc::PointCloudWithNormals(a_T_points_and_normals.first, a_T_points_and_normals.second);
+    pcl::PointCloud<pcl::PointNormal>::Ptr target_cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>());
+    pcl::transformPointCloudWithNormals(*source_cloud_with_normals, *target_cloud_with_normals,
+                                        Eigen::Affine3d(b_T_a.matrix()));
+    const auto noisy_b_T_a = lc::AddNoiseToIsometry3d(b_T_a, translation_stddev, rotation_stddev);
+    const auto estimated_a_T_b =
+      icp.ComputeRelativeTransform(source_cloud_with_normals, target_cloud_with_normals, noisy_b_T_a);
+    ASSERT_TRUE(estimated_a_T_b != boost::none);
+    EXPECT_PRED2(lc::MatrixEquality<2>, estimated_a_T_b->pose.matrix(), b_T_a.inverse().matrix());
+  }
+}
+
+TEST(PointToPlaneICPTester, NoisyInitialEstimateSymmetricCost) {
+  auto params = pc::DefaultPointToPlaneICPParams();
+  params.symmetric_objective = true;
+  params.enforce_same_direction_normals = true;
   constexpr double translation_stddev = 0.01;
   constexpr double rotation_stddev = 0.01;
   pc::PointToPlaneICP<pcl::PointNormal> icp(params);
