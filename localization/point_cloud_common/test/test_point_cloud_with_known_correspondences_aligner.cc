7e5e1f9a6be31be8d29ed5a4a707003f2e319da8
Ryan Soussan <ryan.soussan@gmail.com>
2021-12-02 12:32:42 -0800

    added pwcka random pts test

--- localization/point_cloud_common/test/test_point_cloud_with_known_correspondences_aligner.cc
+++ localization/point_cloud_common/test/test_point_cloud_with_known_correspondences_aligner.cc
@@ -28,13 +28,32 @@
 namespace lc = localization_common;
 namespace pc = point_cloud_common;
 
-TEST(PointCloudWithKnownCorrespondencesAlignerTester, NoisyInitialEstimateCubicPoints) {
+TEST(PointCloudWithKnownCorrespondencesAlignerTester, PointToPointNoisyInitialEstimateCubicPoints) {
   const auto params = pc::DefaultPointCloudWithKnownCorrespondencesAlignerParams();
   constexpr double translation_stddev = 0.01;
   constexpr double rotation_stddev = 0.01;
   pc::PointCloudWithKnownCorrespondencesAligner aligner(params);
   for (int i = 0; i < 50; ++i) {
     const auto source_T_points_and_normals = pc::CubicPoints();
+    const auto target_T_source = lc::RandomIsometry3d();
+    const auto target_T_points_and_normals = lc::TransformPointsWithNormals(
+      source_T_points_and_normals.first, source_T_points_and_normals.second, target_T_source);
+    const auto noisy_target_T_source = lc::AddNoiseToIsometry3d(target_T_source, translation_stddev, rotation_stddev);
+    const auto estimated_target_T_source = aligner.ComputeRelativeTransform(
+      source_T_points_and_normals.first, target_T_points_and_normals.first, noisy_target_T_source);
+    ASSERT_TRUE(estimated_target_T_source != boost::none);
+    EXPECT_PRED2(lc::MatrixEquality<2>, estimated_target_T_source->pose.matrix(), target_T_source.matrix());
+  }
+}
+
+TEST(PointCloudWithKnownCorrespondencesAlignerTester, PointToPointNoisyInitialEstimateRandomPoints) {
+  const auto params = pc::DefaultPointCloudWithKnownCorrespondencesAlignerParams();
+  constexpr double translation_stddev = 0.01;
+  constexpr double rotation_stddev = 0.01;
+  constexpr int num_points = 50;
+  pc::PointCloudWithKnownCorrespondencesAligner aligner(params);
+  for (int i = 0; i < 50; ++i) {
+    const auto source_T_points_and_normals = pc::RandomPointsWithNormals(num_points);
     const auto target_T_source = lc::RandomIsometry3d();
     const auto target_T_points_and_normals = lc::TransformPointsWithNormals(
       source_T_points_and_normals.first, source_T_points_and_normals.second, target_T_source);
