615f77358113ffe9c87f42159bf86f964328ed00
Ryan Soussan <ryan.soussan@gmail.com>
2021-12-14 12:09:16 -0800

    added frame id for depth corr msg

--- localization/depth_odometry/src/utilities.cc
+++ localization/depth_odometry/src/utilities.cc
@@ -36,6 +36,7 @@ ff_msgs::DepthOdometry DepthOdometryMsg(const PoseWithCovarianceAndCorrespondenc
   lc::TimeToMsg(sensor_F_source_T_target.source_time, depth_odometry_msg.odometry.source_time);
   lc::TimeToMsg(sensor_F_source_T_target.target_time, depth_odometry_msg.odometry.target_time);
   depth_odometry_msg.runtime = runtime;
+  depth_odometry_msg.header.frame_id = "haz_cam";
   return depth_odometry_msg;
 }
 
