fb881d8df3bc101b67e0ac4cac42fb39d2cf8bc5
Ryan Soussan <ryan.soussan@gmail.com>
2021-12-20 15:14:07 -0800

    increased atol for depth odom comparison

--- tools/graph_bag/scripts/plot_results.py
+++ tools/graph_bag/scripts/plot_results.py
@@ -866,13 +866,16 @@ def create_plots(
                 sparse_mapping_poses, depth_odom_relative_poses, "Depth Odometry"
             )
             plot_poses(pdf, depth_odom_poses, sparse_mapping_poses, ar_tag_poses)
+            # Use a larger absolute tolerance since depth images and groudtruth use different sensor data 
+            # and have less similar timestamps. Note this timestamp difference reduces the accuracy of depth odometry 
+            # groundtruth comparison.
             plot_loc_state_stats(
                 pdf,
                 depth_odom_poses,
                 sparse_mapping_poses,
                 output_csv_file,
                 "depth_odometry_",
-                0.01,
+                0.1,
                 False,
                 rmse_rel_start_time=rmse_rel_start_time,
                 rmse_rel_end_time=rmse_rel_end_time,
