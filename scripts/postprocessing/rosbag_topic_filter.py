fe76a6c8b2bce64c67b463220e4e63a49c54c0e3
Trey Smith <trey.smith@nasa.gov>
2022-01-27 18:48:55 -0800

    rosbag_topic_filter.py: Improve speed and warn about likely user errors

--- scripts/postprocessing/rosbag_topic_filter.py
+++ scripts/postprocessing/rosbag_topic_filter.py
@@ -37,40 +37,83 @@ Specify multiple --accept or --reject arguments on the command line to add multi
 patterns to the accept or reject list.
 
 Examples:
-  topic_filter.py in.bag -r "/gs/gs_manager/*" out.bag
+  rosbag_topic_filter.py in.bag -r "/gs/gs_manager/*" out.bag
     Filters out rosjava messages produced by Guest Science Manager with malformed
     message definitions that break many rosbag API operations, including the
     command-line utilities 'rosbag filter' and 'rosbag check'.
-  topic_filter.py in.bag -a "/mgt/sys_monitor/time_sync" out.bag
+  rosbag_topic_filter.py in.bag -a "/mgt/sys_monitor/time_sync" out.bag
     Keep only a specific message of interest for data analysis so downstream script
     can run much faster on a smaller bag.
 """
 
+from __future__ import print_function
+
 import argparse
+import collections
 import fnmatch
+import re
 import sys
 
 import rosbag
 
+# check if pattern contains a character that is special for fnmatch
+FNMATCH_SPECIAL_REGEX = re.compile(r"[\*\?\[]")
 
-def topic_filter(inbag_path, accept_patterns, reject_patterns, outbag_path):
+
+def is_special(fnmatch_pattern):
+    return FNMATCH_SPECIAL_REGEX.search(fnmatch_pattern) is not None
+
+
+def topic_filter(verbose, inbag_path, accept_patterns, reject_patterns, outbag_path):
     with rosbag.Bag(inbag_path, "r") as inbag:
-        all_topics = inbag.get_type_and_topic_info()[1].keys()
-        accept_topics = []
-        for topic in all_topics:
-            do_accept = True
-            if accept_patterns:
-                do_accept = any([fnmatch.fnmatch(topic, p) for p in accept_patterns])
-            if reject_patterns:
-                do_accept = do_accept and not any(
-                    [fnmatch.fnmatch(topic, p) for p in reject_patterns]
-                )
-            if do_accept:
-                accept_topics.append(topic)
+        if all([not is_special(p) for p in accept_patterns]) and not reject_patterns:
+            # if the accept patterns are just a list of plain topics and there
+            # are no reject patterns, there's no need to do a slow pass through
+            # the whole bag just to extract what topics it contains
+            accept_topics = accept_patterns
+        else:
+            all_topics = inbag.get_type_and_topic_info()[1].keys()
+            accept_topics = []
+            for topic in all_topics:
+                do_accept = True
+                if accept_patterns:
+                    do_accept = any(
+                        [fnmatch.fnmatch(topic, p) for p in accept_patterns]
+                    )
+                if reject_patterns:
+                    do_accept = do_accept and not any(
+                        [fnmatch.fnmatch(topic, p) for p in reject_patterns]
+                    )
+                if do_accept:
+                    accept_topics.append(topic)
 
+            for p in accept_patterns:
+                if not fnmatch.filter(all_topics, p):
+                    print("warning: no topics in bag match accept pattern '%s'" % p)
+            for p in reject_patterns:
+                if not fnmatch.filter(all_topics, p):
+                    print("warning: no topics in bag match reject pattern '%s'" % p)
+
+        if verbose:
+            print("accept topics:\n  %s" % "\n  ".join(sorted(accept_topics)))
+
+        msg_count = collections.Counter()
         with rosbag.Bag(outbag_path, "w") as outbag:
-            for topic, msg, t in inbag.read_messages(accept_topics):
-                outbag.write(topic, msg, t)
+            if accept_topics:
+                for topic, msg, t in inbag.read_messages(accept_topics):
+                    outbag.write(topic, msg, t)
+                    msg_count[topic] += 1
+            else:
+                # don't call read_messages() with an empty accept_topics list:
+                # turns out, it would iterate through all messages instead of
+                # no messages
+                pass
+
+        for topic in accept_topics:
+            if msg_count[topic] == 0:
+                print("warning: no messages on topic '%s' in bag" % topic)
+        if sum([c for c in msg_count.values()]) == 0:
+            print("warning: output bag does not contain any messages")
 
 
 if __name__ == "__main__":
@@ -82,6 +125,7 @@ if __name__ == "__main__":
         "--accept",
         nargs="?",
         help="Add topic pattern to accept list",
+        default=[],
         action="append",
     )
     parser.add_argument(
@@ -89,10 +133,17 @@ if __name__ == "__main__":
         "--reject",
         nargs="?",
         help="Add topic pattern to reject list",
+        default=[],
         action="append",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Print debug info",
+        action="store_true",
     )
     parser.add_argument("inbag", help="Input bag")
     parser.add_argument("outbag", help="Filtered output bag")
 
     args = parser.parse_args()
-    topic_filter(args.inbag, args.accept, args.reject, args.outbag)
+    topic_filter(args.verbose, args.inbag, args.accept, args.reject, args.outbag)
