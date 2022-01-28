e5c3a34fda42796afebefc559c8730e4d6180384
Trey Smith <trey.smith@nasa.gov>
2022-01-28 11:50:28 -0800

    rosbag_topic_filter.py: tweak docstring

--- scripts/postprocessing/rosbag_topic_filter.py
+++ scripts/postprocessing/rosbag_topic_filter.py
@@ -37,8 +37,8 @@ Specify multiple --accept or --reject arguments on the command line to add multi
 patterns to the accept or reject list.
 
 Examples:
-  rosbag_topic_filter.py in.bag -r "/gs/gs_manager/*" -r /hw/cam_sci/compressed out.bag
-    Filters out rosjava messages produced by Guest Science Manager and SciCamImage APKs
+  rosbag_topic_filter.py in.bag -r "/gs/*" -r /hw/cam_sci/compressed out.bag
+    Filters out rosjava messages produced by Guest Science and SciCamImage APKs
     that have incomplete message definitions that break many rosbag API operations,
     including the command-line utilities 'rosbag filter' and 'rosbag check'.
   rosbag_topic_filter.py in.bag -a /mgt/sys_monitor/time_sync out.bag
