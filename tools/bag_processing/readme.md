dfdd39e868b912e38bf421ed87ba5bf6fc7bfab1
Ryan Soussan <ryan.soussan@gmail.com>
2022-03-02 09:22:30 -0800

    updated doc for check bag for gaps

--- tools/bag_processing/readme.md
+++ tools/bag_processing/readme.md
@@ -4,8 +4,16 @@
 The bag processing package provides several helper tools for handling bagfiles.
 
 # Scripts
-## check\_bags\_for\_gaps
-This is a simple tool to check for large gaps in imu or image data in a bagfile.
+## apply\_histogram\_equalization\_to\_images
+Applies either CLAHE or standard histogram equalization to images in a bagfile.  
+See 'rosrun bag\_processing apply\_histogram\_equalization\_to\_images.py -h'
+for further details and usage instructions.
+
+## check\_bag\_for\_gaps
+Prints gaps above provided maximum time for a bagfile and topic using either message header times or bag receive times.
+Also prints stats for the time differences for messages with the provided topic in the bagfile.
+See 'rosrun bag\_processing check\_bag\_for\_gaps.py -h'
+for further details and usage instructions.
 
 ## merge\_bags
 Merge bags looks for bag files in the current directory with a provided name prefix and merges these into a single bag file.  Bags are assumed to be numbered and are merged in numerical order.
--- tools/bag_processing/scripts/check_bag_for_gaps.py
+++ tools/bag_processing/scripts/check_bag_for_gaps.py
@@ -15,6 +15,11 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+"""
+Checks a bagfile for messages with gaps larger than the provided maximum time difference for the given topic.
+Prints timestamps for found gaps along with min/mean/max/stddev for the time differences.
+"""
+
 import argparse
 import os
 import sys
@@ -23,12 +28,14 @@ import rosbag
 import utilities
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("bagfile")
-    parser.add_argument("topic")
-    parser.add_argument("-m", "--max-time-diff", type=float, default=0.5)
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("bagfile", help="Input bagfile.")
+    parser.add_argument("topic", help="Topic to check.")
+    parser.add_argument("-m", "--max-time-diff", type=float, default=0.5, help="Maximum time difference for a gap, time differences above this will be counted as gaps.")
     # Use header or received time
-    parser.add_argument("-r", "--use-receive-time", action="store_true")
+    parser.add_argument("-r", "--use-receive-time", action="store_true", help="Use receive time instead of header time.")
     args = parser.parse_args()
     if not os.path.isfile(args.bagfile):
         print(("Bag file " + args.bagfile + " does not exist."))
