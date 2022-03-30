2dd607160ceb9f40c9350b4f841e6da2d8f8e6a9
Trey Smith <trey.smith@nasa.gov>
2022-03-30 15:03:53 -0700

    docker workflows consistently use build.sh (#469)
    * docker workflows consistently use build.sh
    
    * make build.sh consistent with run.sh
    
    * fix yaml tabs to spaces

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -18,24 +18,29 @@
 # under the License.
 
 read -r -d '' usage_string <<EOF
-usage: build.sh [-h] [-x] [-b] [-f] [-r] [-d]
-                [target1] [target2] ...
+usage: build.sh [-h] [-x] [-b] [-f] [-r] [-o <owner>] [-d]
+                -- [target1] [target2] ...
 
 -h or --help: Print this help
 -x or --xenial: Build Ubuntu 16.04 docker images
 -b or --bionic: Build Ubuntu 18.04 docker images
 -f or --focal: Build Ubuntu 20.04 docker images
 -r or --remote: Build first target on top of a pre-built remote image
+-o or --owner: Set ghcr.io owner for push action (default: nasa)
 -d or --dry-run: Just print what commands would be run
 
 Build specified docker image targets. Available targets:
 - astrobee_base
 - astrobee
 - test_astrobee
+- push_astrobee_base (push astrobee_base to ghcr.io)
+- push_astrobee (push astrobee to ghcr.io)
 
 Default if no targets are specified: astrobee_base astrobee
 
-Targets are always built in the order listed above.
+Targets are always built in the order listed above. You must
+separately run 'docker login' to authenticate before using build.sh
+to build the push_* targets.
 EOF
 
 set -e
@@ -45,70 +50,104 @@ usage()
     echo "$usage_string"
 }
 
+######################################################################
+# Parse options 1 (validate and normalize with getopt)
+######################################################################
+
+shortopts="h,x,b,f,r,o:,d"
+longopts="help,xenial,bionic,focal,remote,owner:,dry-run"
+opts=$(getopt -a -n build.sh --options "$shortops" --longoptions "$longopts" -- "$@")
+if [ $? -ne 0 ]; then
+    echo
+    usage
+    exit 1
+fi
+# echo "opts: $opts"
+
+eval set -- "$opts"
+
+######################################################################
+# Parse options 2 (extract variables)
+######################################################################
+
 os=`cat /etc/os-release | grep -oP "(?<=VERSION_CODENAME=).*"`
 
 build_astrobee_base="false"
 build_astrobee="false"
 build_test_astrobee="false"
+push_astrobee_base="false"
+push_astrobee="false"
 
 remote="false"
+owner="nasa"
 dry_run="false"
+revision="latest"
 
 while [ "$1" != "" ]; do
     case $1 in
-        -h | --help )                   usage
-                                        exit
-                                        ;;
-        -x | --xenial )                 os="xenial"
-                                        ;;
-        -b | --bionic )                 os="bionic"
-                                        ;;
-        -f | --focal )                  os="focal"
-                                        ;;
-	-r | --remote )                 remote="true"
-					;;
-	-d | --dry-run )                dry_run="true"
-					;;
-	astrobee_base )                 build_astrobee_base="true"
-					;;
-	astrobee )                      build_astrobee="true"
-					;;
-	test_astrobee )                 build_test_astrobee="true"
-					;;
-        * )                             usage
-                                        exit 1
+        --help )                   usage
+                                   exit
+                                   ;;
+        --xenial )                 os="xenial"
+                                   ;;
+        --bionic )                 os="bionic"
+                                   ;;
+        --focal )                  os="focal"
+                                   ;;
+        --remote )                 remote="true"
+                                   ;;
+        --owner )                  owner=$2
+                                   shift
+                                   ;;
+        --dry-run )                dry_run="true"
+                                   ;;
+        -- )                       shift
+                                   break
+                                   ;;
+        * )                        usage
+                                   exit 1
     esac
     shift
 done
 
-if [ "$dry_run" = "true" ]; then
-    echo "Dry run"
+# remaining arguments are positional, i.e., targets
 
-    docker()
-    {
-	# dry run, do nothing
-	{ : ; } 2>/dev/null
-    }
-fi
-
-if [[ "$build_astrobee_base" == "false" \
-	  && "$build_astrobee" == "false" \
-	  && "$build_test_astrobee" == "false" ]]; then
+if [ "$#" -eq 0 ]; then
    # if user didn't specify any targets, set defaults
    build_astrobee_base="true"
    build_astrobee="true"
 fi
 
+# collect user-specified targets
+while [ "$1" != "" ]; do
+    case $1 in
+        astrobee_base )            build_astrobee_base="true"
+                                   ;;
+        astrobee )                 build_astrobee="true"
+                                   ;;
+        test_astrobee )            build_test_astrobee="true"
+                                   ;;
+        push_astrobee_base )       push_astrobee_base="true"
+                                   ;;
+        push_astrobee )            push_astrobee="true"
+                                   ;;
+        * )                        echo "unknown target '$1'"
+                                   usage
+                                   exit 1
+    esac
+    shift
+done
+
 if [[ "$build_astrobee_base" == "true" \
-	  && "$remote" == "true" ]]; then
+          && "$remote" == "true" ]]; then
     echo "Error: --remote doesn't make sense when first target is astrobee_base."
     echo "Run with -h for help."
     exit 1
 fi
 
-script_dir=$(dirname "$(readlink -f "$0")")
-checkout_dir=$(dirname $(dirname "$script_dir"}))
-echo "Astrobee checkout path: "${checkout_dir}/
+######################################################################
+# Set up version
+######################################################################
 
 UBUNTU_VERSION=16.04
 ROS_VERSION=kinetic
@@ -117,63 +156,105 @@ PYTHON=''
 if [ "$os" = "bionic" ]; then
     UBUNTU_VERSION=18.04
     ROS_VERSION=melodic
-    PYTHON=''
+    PYTHON='3'
 elif [ "$os" = "focal" ]; then
     UBUNTU_VERSION=20.04
     ROS_VERSION=noetic
     PYTHON='3'
 fi
-
-remote_repo="ghcr.io/nasa"
-
 echo "Building Ubuntu $UBUNTU_VERSION images"
 
-if [ "$build_astrobee_base" = "true" ]; then
+######################################################################
+# Dry run
+######################################################################
+
+if [ "$dry_run" = "true" ]; then
+    echo "Dry run"
+
+    docker()
+    {
+        # dry run, do nothing
+        { : ; } 2>/dev/null
+    }
+fi
+
+######################################################################
+# Define actions
+######################################################################
+
+build () {
+    stage=$1
+    tag_revision=$2
+    tag_stage=$3
+
     echo ======================================================================
-    echo "Building astrobee_base"
+    echo "Building ${stage}"
+
+    remote_args=""
+    if [ "$remote" = "true" ]; then
+        echo "[Building on top of remote image]"
+        # note: pull from ghcr.io/nasa even if we push to another owner
+        remote_args="--build-arg REMOTE=ghcr.io/nasa"
+        remote="false"  # build subsequent targets on local image
+    fi
+
     set -x
-    docker build ${checkout_dir}/ \
-           -f ${checkout_dir}/scripts/docker/astrobee_base.Dockerfile \
+    docker build . \
+           -f ./scripts/docker/${stage}.Dockerfile \
            --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
            --build-arg ROS_VERSION=${ROS_VERSION} \
            --build-arg PYTHON=${PYTHON} \
-           -t astrobee/astrobee:latest-base-ubuntu${UBUNTU_VERSION}
+           $remote_args \
+           -t astrobee/astrobee:${tag_revision}${tag_stage}ubuntu${UBUNTU_VERSION}
     { set +x; } 2>/dev/null
+}
+
+push () {
+    stage=$1
+    tag_revision=$2
+    tag_stage=$3
+
+    tag_suffix="astrobee:${tag_revision}${tag_stage}ubuntu${UBUNTU_VERSION}"
+    local_tag="astrobee/$tag_suffix"
+    remote_tag="ghcr.io/${owner}/$tag_suffix"
+
+    echo ======================================================================
+    echo "Pushing ${stage}"
+
+    set -x
+    docker tag $local_tag $remote_tag
+    docker push $remote_tag
+    { set +x; } 2>/dev/null
+}
+
+######################################################################
+# Run
+######################################################################
+
+script_dir=$(dirname "$(readlink -f "$0")")
+checkout_dir=$(dirname $(dirname "$script_dir"}))
+echo "Astrobee checkout path: "${checkout_dir}/
+
+set -x
+cd "${checkout_dir}"
+{ set +x; } 2>/dev/null
+
+if [ "$build_astrobee_base" = "true" ]; then
+    build astrobee_base "${revision}-" "base-"
 fi
 
 if [ "$build_astrobee" = "true" ]; then
-    echo ======================================================================
-    echo "Building astrobee"
-    remote_args=""
-    if [ "$remote" = "true" ]; then
-	echo "[Building on top of remote image]"
-	remote_args="--build-arg REMOTE=${remote_repo}"
-	remote="false"  # build subsequent targets on local image
-    fi
-    set -x
-    docker build ${checkout_dir}/ \
-           -f ${checkout_dir}/scripts/docker/astrobee.Dockerfile \
-           --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
-           --build-arg ROS_VERSION=${ROS_VERSION} \
-	   $remote_args \
-           -t astrobee/astrobee:latest-ubuntu${UBUNTU_VERSION}
-    { set +x; } 2>/dev/null
+    build astrobee "${revision}-" ""
 fi
 
 if [ "$build_test_astrobee" = "true" ]; then
-    echo ======================================================================
-    echo "Building test_astrobee"
-    remote_args=""
-    if [ "$remote" = "true" ]; then
-	echo "[Building on top of remote image]"
-	remote_args="--build-arg REMOTE=${remote_repo}"
-	remote="false"  # build subsequent targets on local image
-    fi
-    set -x
-    docker build ${checkout_dir}/ \
-           -f ${checkout_dir}/scripts/docker/test_astrobee.Dockerfile \
-           --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
-	   $remote_args \
-           -t astrobee/astrobee:test-ubuntu${UBUNTU_VERSION}
-    { set +x; } 2>/dev/null
+    build test_astrobee "" "test-"
+fi
+
+if [ "$push_astrobee_base" = "true" ]; then
+    push astrobee_base "${revision}-" "base-"
+fi
+
+if [ "$push_astrobee" = "true" ]; then
+    push astrobee "${revision}-" ""
 fi
