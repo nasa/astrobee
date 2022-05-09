00247748061d19a02c3160fa1c024e578347bc5f
Ryan Soussan <ryan.soussan@gmail.com>
2022-05-09 14:04:40 -0700

    moved make surf map to sparse mapping

---
+++ localization/localization_common/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['localization_common'],
+    scripts=['scripts/utilities.py'],
+    package_dir={'': 'scripts'}
+)
+
+setup(**d)
