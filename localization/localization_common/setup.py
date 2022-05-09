825b5cc5cf18731ca3b81cd575054aa06aa8bb0d
Ryan Soussan <ryan.soussan@gmail.com>
2022-05-09 15:21:13 -0700

    black update

--- localization/localization_common/setup.py
+++ localization/localization_common/setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['localization_common'],
-    package_dir={'': 'scripts'}
+    packages=["localization_common"], package_dir={"": "scripts"}
 )
 
 setup(**d)
