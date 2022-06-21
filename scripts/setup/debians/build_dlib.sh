0c0a718037842b9c5cbb90f26c0668e0a241b0ff
Ruben Garcia <84038639+rgarciaruiz@users.noreply.github.com>
2022-06-21 13:30:03 -0700

    Ubuntu20 migration (#511)
    * Add support for ubuntu 20.04 cross-compilation
    * A patch is required to compile our custom debians on armhf due to
    a bug on CMake 3.16.3. This can also be used for amd64.

--- scripts/setup/debians/build_dlib.sh
+++ scripts/setup/debians/build_dlib.sh
@@ -3,6 +3,7 @@
 PACKAGE_NAME=libdbowdlib
 ORIG_TAR=libdbowdlib_0.1.orig.tar.gz
 DEB_DIR=dlib
+DIST=$(grep -oP "(?<=VERSION_CODENAME=).*" /etc/os-release)
 
 if [ -d $PACKAGE_NAME ]; then
   rm -rf $PACKAGE_NAME
@@ -11,5 +12,6 @@ git clone --quiet https://github.com/dorian3d/DLib.git $PACKAGE_NAME --branch v1
 cd $PACKAGE_NAME
 git archive --prefix=$PACKAGE_NAME/ --output=../$ORIG_TAR --format tar.gz HEAD || exit 1
 cp -r ../$DEB_DIR debian
+dch -l"+$DIST" -D"$DIST" "Set distribution '$DIST' for local build"
 debuild -us -uc || exit 1
 cd ..
--- scripts/setup/debians/build_openmvg.sh
+++ scripts/setup/debians/build_openmvg.sh
@@ -2,15 +2,15 @@
 #
 # Copyright (c) 2017, United States Government, as represented by the
 # Administrator of the National Aeronautics and Space Administration.
-# 
+#
 # All rights reserved.
-# 
+#
 # The Astrobee platform is licensed under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with the
 # License. You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,6 +20,7 @@
 PACKAGE_NAME=libopenmvg
 ORIG_TAR=libopenmvg_1.1.orig.tar.gz
 DEB_DIR=openmvg
+DIST=$(grep -oP "(?<=VERSION_CODENAME=).*" /etc/os-release)
 
 if [ -d $PACKAGE_NAME ]; then
   rm -rf $PACKAGE_NAME
@@ -28,5 +29,6 @@ git clone --quiet https://github.com/openMVG/openMVG.git $PACKAGE_NAME --branch 
 cd $PACKAGE_NAME
 git archive --prefix=$PACKAGE_NAME/ --output=../$ORIG_TAR --format tar.gz HEAD || exit 1
 cp -r ../$DEB_DIR debian
+dch -l"+$DIST" -D"$DIST" "Set distribution '$DIST' for local build"
 debuild -us -uc || exit 1
 cd ..
