752d83b5897817eda8c9d9e48a4e839f44697d9f
Trey Smith <trey.smith@nasa.gov>
2022-11-10 07:20:57 -0800

    Update code auto-generation scripts and docs workflow (#570)
    - Scripts support Python 3
    - Enables running docs workflow under Ubuntu 20.04
    - Enables faster doxygen install with apt-get

--- scripts/build/luaTable.py
+++ scripts/build/luaTable.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2017, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+#
+# All rights reserved.
+#
+# The Astrobee platform is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 """
 Output Python data structure as a Lua table constructor.
@@ -10,14 +26,15 @@ is no easy way to guarantee the python 3 compatibility changes
 continue to generate the fsw files correctly.
 """
 
-import sys
+try:
+    from cStringIO import StringIO  # fmt: skip
+except ImportError:
+    # Python 3
+    from io import StringIO
 
-if (sys.version_info > (3, 0)):
-    # exit if python 3
-    print("The lua table script is only compatible with python 2")
-    sys.exit(1)
-
-from cStringIO import StringIO  # fmt: skip
+if not hasattr(__builtins__, "basestring"):
+    # Python 3
+    basestring = (str, bytes)
 
 
 def q(s):
@@ -25,34 +42,36 @@ def q(s):
 
 
 def ind(lvl):
-    return ' ' * (lvl * 2)
+    return " " * (lvl * 2)
 
 
 def dumpStream(out, d, lvl=0):
     def w(s):
         out.write(s)
 
-    if isinstance(d, basestring): # fmt: skip
+    if isinstance(d, basestring):
+
         w(q(d))
+    # fmt: skip
 
     elif isinstance(d, (list, tuple)):
         if d:
-            w('{\n')
+            w("{\n")
             n = len(d)
             for i, elt in enumerate(d):
                 w(ind(lvl + 1))
                 dumpStream(out, elt, lvl + 1)
                 if i < n - 1:
-                    w(',')
-                w('\n')
+                    w(",")
+                w("\n")
             w(ind(lvl))
-            w('}')
+            w("}")
         else:
-            w('{}')
+            w("{}")
 
     elif isinstance(d, dict):
         if d:
-            w('{\n')
+            w("{\n")
             n = len(d)
             keys = list(d.keys())
             keys.sort()
@@ -60,15 +79,15 @@ def dumpStream(out, d, lvl=0):
                 v = d[k]
                 w(ind(lvl + 1))
                 w(k)
-                w('=')
+                w("=")
                 dumpStream(out, v, lvl + 1)
                 if i < n - 1:
-                    w(',')
-                w('\n')
+                    w(",")
+                w("\n")
             w(ind(lvl))
-            w('}')
+            w("}")
         else:
-            w('{}')
+            w("{}")
 
     else:
         w(str(d))
