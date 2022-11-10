19bd0e5bbe191a09848ec20681b20d410b49ee6e
Trey Smith <trey.smith@nasa.gov>
2022-11-10 07:20:57 -0800

    Update code auto-generation scripts and docs workflow (#570)
    - Scripts support Python 3
    - Enables running docs workflow under Ubuntu 20.04
    - Enables faster doxygen install with apt-get

--- scripts/build/xpjsonAstrobee.py
+++ scripts/build/xpjsonAstrobee.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2017, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+#
+# All rights reserved.
+#
+# The Astrobee platform is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 """
 Common configuration for working with XPJSON and RAPID.
@@ -8,20 +24,20 @@ import re
 from xgds_planner2 import xpjson
 
 XPJSON_PARAM_VALUE_TYPE_MAPPINGS = {
-    'boolean': 'RAPID_BOOL',
-    'double': 'RAPID_DOUBLE',
-    'float': 'RAPID_FLOAT',
-    'long': 'RAPID_INT',
-    'long long': 'RAPID_LONG_LONG',
-    'string': 'RAPID_STRING',
-    'array[3].double': 'RAPID_VEC3d',
-    'Point': 'RAPID_VEC3d',
-    'array[9].float': 'RAPID_MAT33f',
-    'quaternion': 'RAPID_MAT33f',
+    "boolean": "RAPID_BOOL",
+    "double": "RAPID_DOUBLE",
+    "float": "RAPID_FLOAT",
+    "long": "RAPID_INT",
+    "long long": "RAPID_LONG_LONG",
+    "string": "RAPID_STRING",
+    "array[3].double": "RAPID_VEC3d",
+    "Point": "RAPID_VEC3d",
+    "array[9].float": "RAPID_MAT33f",
+    "quaternion": "RAPID_MAT33f",
 }
 
-CAMEL_CASE_WORD_BOUNDARY_REGEX = re.compile(r'([a-z])([A-Z])')
-NON_IDENT_CHARACTER_REGEX = re.compile(r'[^a-zA-Z0-9_]')
+CAMEL_CASE_WORD_BOUNDARY_REGEX = re.compile(r"([a-z])([A-Z])")
+NON_IDENT_CHARACTER_REGEX = re.compile(r"[^a-zA-Z0-9_]")
 
 
 def fixName(s):
@@ -30,16 +46,17 @@ def fixName(s):
     which is a workaround for 'name' being a reserved word in XPJSON.
     For backward compatibility, we convert 'name_' to 'name' in some cases.
     """
-    if s == 'name_':
-        return 'name'
+    if s == "name_":
+        return "name"
     else:
         return s
 
 
 def allCaps(s):
-    s = re.sub(CAMEL_CASE_WORD_BOUNDARY_REGEX,
-               lambda m: m.group(1) + '_' + m.group(2), s)
-    s = re.sub(NON_IDENT_CHARACTER_REGEX, '_', s)
+    s = re.sub(
+        CAMEL_CASE_WORD_BOUNDARY_REGEX, lambda m: m.group(1) + "_" + m.group(2), s
+    )
+    s = re.sub(NON_IDENT_CHARACTER_REGEX, "_", s)
     s = s.upper()
     return s
 
