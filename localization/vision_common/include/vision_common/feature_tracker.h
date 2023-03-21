43336f53109dd1c1ded35180c378c6b0a4fd77c4
Ryan Soussan <ryan.soussan@gmail.com>
2023-03-21 15:59:52 -0700

    Revert "updated feature tracker to be header only"
    This reverts commit 12cfcb01013a6c0c4f81f404a62b34f42ce6dedf.

--- localization/vision_common/include/vision_common/feature_tracker.h
+++ localization/vision_common/include/vision_common/feature_tracker.h
@@ -24,15 +24,13 @@
 #include <vision_common/feature_track.h>
 #include <vision_common/feature_tracker_params.h>
 
-#include <map>
 #include <unordered_map>
 #include <vector>
 
 namespace vision_common {
-template <typename FeatureTrackType = FeatureTrack>
+using IdFeatureTrackMap = std::unordered_map<FeatureId, FeatureTrack>;
 class FeatureTracker {
  public:
-  using IdFeatureTrackMap = std::unordered_map<FeatureId, FeatureTrackType>;
   explicit FeatureTracker(const FeatureTrackerParams& params);
 
   // Default constructor only for serialization
@@ -52,7 +50,7 @@ class FeatureTracker {
   const IdFeatureTrackMap& feature_tracks() const;
 
   // Returns feature track references ordered from longest to shortest
-  std::vector<std::reference_wrapper<const FeatureTrackType>> FeatureTracksLengthOrdered() const;
+  std::vector<std::reference_wrapper<const FeatureTrack>> FeatureTracksLengthOrdered() const;
 
   // Returns the number of feature tracks.
   size_t size() const;
@@ -85,111 +83,6 @@ class FeatureTracker {
   IdFeatureTrackMap id_feature_track_map_;
   FeatureTrackerParams params_;
 };
-
-// Implementation
-template <typename FeatureTrackType>
-FeatureTracker<FeatureTrackType>::FeatureTracker(const FeatureTrackerParams& params) : params_(params) {}
-
-template <typename FeatureTrackType>
-void FeatureTracker<FeatureTrackType>::Update(const FeaturePoints& feature_points) {
-  if (feature_points.empty() && params_.remove_undetected_feature_tracks) {
-    Clear();
-    LogDebug("UpdateFeatureTracks: Removed all feature tracks.");
-    return;
-  }
-
-  const int starting_num_feature_tracks = size();
-  LogDebug("UpdateFeatureTracks: Starting num feature tracks: " << starting_num_feature_tracks);
-  for (const auto& feature_point : feature_points) {
-    AddOrUpdateTrack(feature_point);
-  }
-  const int post_add_num_feature_tracks = size();
-  LogDebug("UpdateFeatureTracks: Added feature tracks: " << post_add_num_feature_tracks - starting_num_feature_tracks);
-
-  if (params_.remove_undetected_feature_tracks) {
-    const auto feature_points_timestamp = feature_points.front().timestamp;
-    RemoveUndetectedFeatureTracks(feature_points_timestamp);
-  }
-  const int removed_num_feature_tracks = post_add_num_feature_tracks - size();
-  LogDebug("UpdateFeatureTracks: Removed feature tracks: " << removed_num_feature_tracks);
-  LogDebug("UpdateFeatureTracks: Final total num feature tracks: " << size());
-}
-
-template <typename FeatureTrackType>
-void FeatureTracker<FeatureTrackType>::RemoveOldPoints(const localization_common::Time oldest_allowed_time) {
-  for (auto& feature_track : id_feature_track_map_) {
-    feature_track.second.RemoveOldValues(oldest_allowed_time);
-  }
-  RemoveEmptyFeatureTracks();
-}
-
-template <typename FeatureTrackType>
-const typename FeatureTracker<FeatureTrackType>::IdFeatureTrackMap& FeatureTracker<FeatureTrackType>::feature_tracks()
-  const {
-  return id_feature_track_map_;
-}
-
-template <typename FeatureTrackType>
-std::vector<std::reference_wrapper<const FeatureTrackType>>
-FeatureTracker<FeatureTrackType>::FeatureTracksLengthOrdered() const {
-  std::map<int, int> length_id_map;
-  for (const auto& feature_track : feature_tracks()) {
-    length_id_map.insert({feature_track.second.size(), feature_track.first});
-  }
-  std::vector<std::reference_wrapper<const FeatureTrack>> feature_tracks_length_ordered;
-  // Add tracks in order from longest to shortest
-  for (auto it = length_id_map.crbegin(); it != length_id_map.crend(); ++it) {
-    feature_tracks_length_ordered.emplace_back((this->feature_tracks().at(it->second)));
-  }
-
-  return feature_tracks_length_ordered;
-}
-
-template <typename FeatureTrackType>
-size_t FeatureTracker<FeatureTrackType>::size() const {
-  return id_feature_track_map_.size();
-}
-
-template <typename FeatureTrackType>
-bool FeatureTracker<FeatureTrackType>::empty() const {
-  return id_feature_track_map_.empty();
-}
-
-template <typename FeatureTrackType>
-void FeatureTracker<FeatureTrackType>::Clear() {
-  id_feature_track_map_.clear();
-}
-
-template <typename FeatureTrackType>
-void FeatureTracker<FeatureTrackType>::AddOrUpdateTrack(const FeaturePoint& feature_point) {
-  if (id_feature_track_map_.count(feature_point.feature_track_id) == 0) {
-    id_feature_track_map_[feature_point.feature_track_id] = FeatureTrack(feature_point.feature_track_id);
-  }
-  id_feature_track_map_[feature_point.feature_track_id].Add(feature_point.timestamp, feature_point);
-}
-
-template <typename FeatureTrackType>
-void FeatureTracker<FeatureTrackType>::RemoveUndetectedFeatureTracks(
-  const localization_common::Time& feature_point_timestamp) {
-  for (auto feature_it = id_feature_track_map_.cbegin(); feature_it != id_feature_track_map_.cend();) {
-    if (!feature_it->second.Contains(feature_point_timestamp)) {
-      feature_it = id_feature_track_map_.erase(feature_it);
-    } else {
-      ++feature_it;
-    }
-  }
-}
-
-template <typename FeatureTrackType>
-void FeatureTracker<FeatureTrackType>::RemoveEmptyFeatureTracks() {
-  for (auto feature_it = id_feature_track_map_.cbegin(); feature_it != id_feature_track_map_.cend();) {
-    if (feature_it->second.empty()) {
-      feature_it = id_feature_track_map_.erase(feature_it);
-    } else {
-      ++feature_it;
-    }
-  }
-}
 }  // namespace vision_common
 
 #endif  // VISION_COMMON_FEATURE_TRACKER_H_
