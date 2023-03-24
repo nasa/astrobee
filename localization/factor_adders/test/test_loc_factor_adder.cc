de259b3a6f675449e7482e094e711e02b6dfdffa
Ryan Soussan <ryan.soussan@gmail.com>
2023-03-24 15:51:45 -0700

    updated loc factor adder test

--- localization/factor_adders/test/test_loc_factor_adder.cc
+++ localization/factor_adders/test/test_loc_factor_adder.cc
@@ -135,7 +135,8 @@ class LocFactorAdderTest : public ::testing::Test {
   void EXPECT_SAME_NOISE_NON_ROBUST(const int factor_index, const gtsam::SharedNoiseModel& noise) {
     const auto projection_factor = dynamic_cast<gtsam::LocProjectionFactor<>*>(factors_[factor_index].get());
     ASSERT_TRUE(projection_factor);
-    const auto factor_noise = dynamic_cast<gtsam::noiseModel::Gaussian*>(projection_factor)->covariance();
+    const auto factor_noise =
+      dynamic_cast<gtsam::noiseModel::Gaussian*>(projection_factor->noiseModel().get())->covariance();
     const auto expected_noise = dynamic_cast<gtsam::noiseModel::Gaussian*>(noise.get())->covariance();
     EXPECT_MATRIX_NEAR(factor_noise, expected_noise, 1e-6);
   }
@@ -262,6 +263,7 @@ TEST_F(LocFactorAdderTest, ScaleProjectionNoiseWithNumLandmarks) {
 TEST_F(LocFactorAdderTest, ScaleProjectionNoiseWithLandmarkDistance) {
   auto params = DefaultParams();
   params.add_projection_factors = true;
+  params.scale_projection_noise_with_landmark_distance = true;
   Initialize(params);
   factor_adder_->AddMeasurement(measurements_[0]);
   // Add first factors
@@ -277,7 +279,29 @@ TEST_F(LocFactorAdderTest, ScaleProjectionNoiseWithLandmarkDistance) {
     // Don't use robust cost here to more effectively correct a drift occurance
     const auto expected_noise = gtsam::SharedIsotropic(
       gtsam::noiseModel::Isotropic::Sigma(2, params_.projection_noise_scale * 1.0 / nav_cam_t_landmark.z()));
-    EXPECT_SAME_NOISE(0, expected_noise);
+    EXPECT_SAME_NOISE_NON_ROBUST(0, expected_noise);
+  }
+  // Check second factor noise
+  {
+    const Eigen::Vector3d& world_t_landmark = measurements_[0].matched_projections[1].map_point;
+    const Eigen::Isometry3d nav_cam_T_world =
+      localization_common::EigenPose(*world_T_body * params_.body_T_cam).inverse();
+    const gtsam::Point3 nav_cam_t_landmark = nav_cam_T_world * world_t_landmark;
+    // Don't use robust cost here to more effectively correct a drift occurance
+    const auto expected_noise = gtsam::SharedIsotropic(
+      gtsam::noiseModel::Isotropic::Sigma(2, params_.projection_noise_scale * 1.0 / nav_cam_t_landmark.z()));
+    EXPECT_SAME_NOISE_NON_ROBUST(1, expected_noise);
+  }
+  // Check third factor noise
+  {
+    const Eigen::Vector3d& world_t_landmark = measurements_[0].matched_projections[2].map_point;
+    const Eigen::Isometry3d nav_cam_T_world =
+      localization_common::EigenPose(*world_T_body * params_.body_T_cam).inverse();
+    const gtsam::Point3 nav_cam_t_landmark = nav_cam_T_world * world_t_landmark;
+    // Don't use robust cost here to more effectively correct a drift occurance
+    const auto expected_noise = gtsam::SharedIsotropic(
+      gtsam::noiseModel::Isotropic::Sigma(2, params_.projection_noise_scale * 1.0 / nav_cam_t_landmark.z()));
+    EXPECT_SAME_NOISE_NON_ROBUST(2, expected_noise);
   }
 }
 
