f09806f021db65fc2f0459c6bc5988b972ffc48b
Ryan Soussan <ryan.soussan@gmail.com>
2023-04-08 09:23:55 -0700

    added ros graph/vio param loading

---
+++ localization/parameter_reader/src/ros_graph_localizer.cc
@@ -0,0 +1,31 @@
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <parameter_reader/ros_graph_localizer.h>
+#include <msg_conversions/msg_conversions.h>
+
+namespace parameter_reader {
+namespace mc = msg_conversions;
+namespace rl = ros_graph_localizer;
+
+void LoadRosGraphLocalizerNodeletParams(config_reader::ConfigReader& config, rl::RosGraphLocalizerNodeletParams& params,
+                              const std::string& prefix) {
+  LOAD_PARAM(params.max_graph_vio_state_buffer_size, config, prefix);
+  LOAD_PARAM(params.max_vl_matched_projections_buffer_size, config, prefix);
+}
+}  // namespace parameter_reader
---
+++ localization/parameter_reader/src/ros_graph_vio.cc
@@ -0,0 +1,39 @@
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <parameter_reader/imu_integration.h>
+#include <parameter_reader/ros_graph_vio.h>
+#include <msg_conversions/msg_conversions.h>
+
+namespace parameter_reader {
+namespace mc = msg_conversions;
+namespace rv = ros_graph_vio;
+
+void LoadImuBiasInitializerParams(config_reader::ConfigReader& config, rv::ImuBiasInitializerParams& params,
+                              const std::string& prefix) {
+  LoadImuFilterParams(config, params.filter, prefix);
+  LOAD_PARAM(params.imu_bias_filename, config, prefix);
+  LOAD_PARAM(params.num_bias_estimation_measurements, config, prefix);
+}
+
+void LoadRosGraphVIONodeletParams(config_reader::ConfigReader& config, rv::RosGraphVIONodeletParams& params,
+                              const std::string& prefix) {
+  LOAD_PARAM(params.max_imu_buffer_size, config, prefix);
+  LOAD_PARAM(params.max_feature_point_buffer_size, config, prefix);
+}
+}  // namespace parameter_reader
