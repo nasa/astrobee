ee63c16a91744f9c8c5d7d08b7c9ab17d4e46069
Luisa Mao <luisa.maocp@gmail.com>
2023-06-15 15:19:44 -0700

    lowered min landmarks for surf

--- localization/sparse_mapping/src/sparse_map.cc
+++ localization/sparse_mapping/src/sparse_map.cc
@@ -689,7 +689,7 @@ bool Localize(cv::Mat const& test_descriptors,
   }
 
   // To turn on verbose localization for debugging
-  // google::SetCommandLineOption("verbose_localization", "true");
+  google::SetCommandLineOption("verbose_localization", "true");
 
   // Find matches to each image in map. Do this in two passes. First,
   // find matches to all map images, then keep only num_similar
