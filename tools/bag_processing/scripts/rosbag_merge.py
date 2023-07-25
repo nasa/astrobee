8dbc6520fc54e08ddf45a0a26073b6a847ff2035
Marina Moreira <marina.moreira@nasa.gov>
2023-07-25 09:57:35 -0700

    fixing dependencies issues

--- tools/bag_processing/scripts/rosbag_merge.py
+++ tools/bag_processing/scripts/rosbag_merge.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright (c) 2017, United States Government, as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 #
