9656cdff6635ea2742fba4311834866a358157c9
Marina Moreira <marina.moreira@nasa.gov>
2024-04-10 11:35:46 -0700

    removing docker remote folder and just have the vscode method

--- scripts/docker/remote/exec_vnc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ${HOME}/ros_ws/astrobee/src
-docker exec -it --env "DISPLAY=:0" astrobee-vnc bash
--- scripts/docker/remote/run_vnc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd ${HOME}/ros_ws/astrobee/src
-
-docker run -ti --rm -p 9090:22/tcp --name astrobee-vnc astrobee/astrobee:latest-vnc-ubuntu20.04
