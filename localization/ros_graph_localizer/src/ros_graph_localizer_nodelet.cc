e7b0c60ba0329c4dffbe7a079231b3a240171728
Ryan Soussan <ryan.soussan@gmail.com>
2024-05-14 14:09:06 -0700

    prevent double publishing world_T_body tf

--- localization/ros_graph_localizer/src/ros_graph_localizer_nodelet.cc
+++ localization/ros_graph_localizer/src/ros_graph_localizer_nodelet.cc
@@ -192,7 +192,7 @@ void RosGraphLocalizerNodelet::PublishGraphLocalizerMessages() {
 
   // TODO(rsoussan): Only publish if things have changed?
   PublishGraphLocalizerState();
-  PublishWorldTBodyTF();
+  // PublishWorldTBodyTF();
 }
 
 void RosGraphLocalizerNodelet::PublishWorldTBodyTF() {
@@ -205,6 +205,7 @@ void RosGraphLocalizerNodelet::PublishWorldTBodyTF() {
 
   const auto world_T_body_tf = lc::PoseToTF(*latest_pose, "world", "body", *latest_timestamp, platform_name_);
   if (world_T_body_tf.header.stamp == last_tf_body_time_) return;
+
   last_tf_body_time_ = world_T_body_tf.header.stamp;
   transform_pub_.sendTransform(world_T_body_tf);
 }
