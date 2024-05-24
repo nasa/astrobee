8650e9013451e282deda50d870bc3b83d13f5245
Ryan Soussan <ryan.soussan@gmail.com>
2024-05-24 16:49:41 -0700

    fixed missing estimating bias in graph vio msg

--- localization/ros_graph_vio/src/ros_graph_vio_wrapper.cc
+++ localization/ros_graph_vio/src/ros_graph_vio_wrapper.cc
@@ -170,6 +170,7 @@ boost::optional<ff_msgs::GraphVIOState> RosGraphVIOWrapper::GraphVIOStateMsg() {
   lc::TimeToHeader(*(nodes.LatestTimestamp()), msg.header);
   msg.child_frame_id = "odom";
   msg.standstill = graph_vio_->standstill();
+  msg.estimating_bias = !Initialized() && !imu_bias_initializer_->Bias();
   msg.num_detected_of_features = graph_vio_->feature_tracker().size();
   msg.num_of_factors = graph_vio_->NumFactors<factor_adders::RobustSmartFactor>();
   msg.num_depth_factors = graph_vio_->NumFactors<gtsam::PointToPointBetweenFactor>();
