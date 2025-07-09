ae4e06f05c8be2399a0b48f6ac6070de85461fb7
Emily <emily.j.zimmerman@nasa.gov>
2025-07-09 14:41:47 -0700

    adding conversion to csv file

---
+++ tools/localization_analysis/scripts/bag_to_csv.py
@@ -0,0 +1,29 @@
+import rosbag
+from geometry_msgs.msg import PoseArray, Pose, Point, Quaternion
+import csv
+import sys
+ 
+input_bag = rosbag.Bag(sys.argv[1])
+mapping_pose = []
+for topic, msg, t in input_bag.read_messages(topics="/sparse_mapping/pose"):
+    time = msg.header.time
+    for i, pose in enumerate(msg.poses):
+        point_x = pose.postion.x
+        point_y = pose.postion.y
+        point_z = pose.postion.z
+        ori_x = pose.orientation.x
+        ori_y = pose.orientation.y
+        ori_z = pose.orientation.z
+        ori_w = pose.orientation.w
+        info = (time, point_x, point_y, point_z, ori_x, ori_y, ori_z, ori_w)
+        mapping_pose.append(info)
+ 
+poses_file = "pose_file.csv"
+ 
+with open(poses_file, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["time", "posex", "posey", "posez", "orix", "oriy", "oriz", "oriw"])
+    for tup in mapping_pose:
+        writer.writerow(tup)
+ 
+print("all done")
