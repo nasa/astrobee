b932329825186eed3763de084b116b4475d2a694
Cmaso010 <cmaso010@gmail.com>
2025-07-18 12:07:02 -0700

    Update bag_to_csv.py

--- tools/localization_analysis/scripts/bag_to_csv.py
+++ tools/localization_analysis/scripts/bag_to_csv.py
@@ -22,21 +22,20 @@ input_bag = rosbag.Bag(args.bag)
 mapping_pose = []
 for topic, msg, t in input_bag.read_messages(topics="/sparse_mapping/pose"):
     time = msg.header.stamp
-    for i, pose in enumerate(msg.poses):
-        point_x = pose.position.x
-        point_y = pose.position.y
-        point_z = pose.position.z
-        ori_x = pose.orientation.x
-        ori_y = pose.orientation.y
-        ori_z = pose.orientation.z
-        ori_w = pose.orientation.w
-        info = (time, point_x, point_y, point_z, ori_x, ori_y, ori_z, ori_w)
-        mapping_pose.append(info)
+    point_x = msg.pose.position.x
+    point_y = msg.pose.position.y
+    point_z = msg.pose.position.z
+    ori_x = msg.pose.orientation.x
+    ori_y = msg.pose.orientation.y
+    ori_z = msg.pose.orientation.z
+    ori_w = msg.pose.orientation.w
+    info = (time, point_x, point_y, point_z, ori_x, ori_y, ori_z, ori_w)
+    mapping_pose.append(info)
 
 if args.file_name is not None:
     poses_file = "pose_" + args.file_name + ".csv"
 else:
-    split = input_bag.split(".", 1)
+    split = args.bag.split(".", 1)
     file_name = split[0]
     poses_file = file_name + ".csv"
 
